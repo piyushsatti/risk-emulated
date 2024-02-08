7e3972b5b90468ead011511fc2972248afe03453
piyushsatti <piyushsatti@gmail.com>
2024-02-08 16:43:09 -0500

    Yet another .gitignore update.

--- WARZONE/src/main/java/views/SampleViews.java
+++ WARZONE/src/main/java/views/SampleViews.java
@@ -2,6 +2,6 @@ package Build_1.Views;
 
 public class SampleViews {
  public static void main(String[] args) {
-		System.out.println("This is Sample Class of Views Folder!");
+     System.out.println("This is Sample Class of Views Folder!");
   }
 }
