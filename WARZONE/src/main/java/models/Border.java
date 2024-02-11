e53df65df1514eb37b6775f76bf4fbfd5fb322af
Eden Almakias <eden.almakias@gmail.com>
2024-02-11 15:22:37 -0500

    Some outline of the potential data structures we will use. Necessary to discuss implementation further before finalizing implementation

---
+++ WARZONE/src/main/java/models/Border.java
@@ -0,0 +1,9 @@
+package models;
+
+/**
+ * Class representing the borders of a country.
+ * Contains variable d_target pointing to a single neighbor
+ */
+public class Border {
+    public Country d_target;
+}
---
+++ WARZONE/src/main/java/models/Map.java
@@ -0,0 +1,13 @@
+package models;
+
+import java.util.List;
+
+/**
+ * Class representing the Warzone map
+ * Contains d_countryList which is a list containing all the countries
+ * on the map
+ */
+public class Map {
+
+    public List<Country> d_countryList;
+}
