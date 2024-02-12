e31ce04867ea0f508b2b8e8b254ab39238a40544
Eden Almakias <eden.almakias@gmail.com>
2024-02-12 14:19:14 -0500

    Added Continent class and Map constructor

---
+++ WARZONE/src/main/java/models/Continent.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Continent {
+    /**
+     * Name of continent
+     */
+    public String d_continentName;
+
+    /**
+     * Value of bonus associated with control of the continent
+     */
+    public int bonus;
+
+    public Continent(String name, int bonusValue){
+        this.d_continentName = name;
+        this.bonus = bonusValue;
+    }
+
+}
