8216afccc00e5cb1e9444404872a0fab342db138
priya <priyanshuadhikari7@gmail.com>
2024-02-12 23:57:32 +0530

    Adding basic layout to player class

--- WARZONE/src/main/java/models/Player.java
+++ WARZONE/src/main/java/models/Player.java
@@ -1,25 +1,46 @@
 package models;
 import java.util.*;
+
 public class Player {
 
-    private String d_name;
+    private String d_playerName;
     private int d_reinforcements;
-    private List<Country> d_assignedCountries;
-
+    private ArrayList<Country> d_assignedCountries;
+    
+    //Getters
     public String getName() {
-        return d_name;
-    }
-
-    public void setName(String p_name) {
-        this.d_name = p_name;
+        return d_playerName;
     }
     public int getReinforcements() {
         return d_reinforcements;
     }
-
-    public void setReinforcements(int p_reinforcements) {
-        this.d_reinforcements = p_reinforcements;
+    public ArrayList<Country> getassignedCountries() {
+        return d_assignedCountries;
     }
+    
+    //Setters
+    public void setName(String p_name) {
+        this.d_playerName= p_name;
+    }
+    public void setReinforcements(int p_reinforcements) {
+        this.d_reinforcements= p_reinforcements;
+    }
+    public void setassignedCountries(ArrayList<Country> d_assignedCountries) {
+        this.d_assignedCountries= d_assignedCountries;
+    }
+    
+    //Constructors
+    Player(){
+     d_playerName= "";
+     d_reinforcements= 0;
+     d_assignedCountries= new ArrayList<>(); 	
+    }
+    
+    Player(String d_playerName,int d_reinforcements,ArrayList<Country> d_assignedCountries;){
+        this.d_playerName= "";
+        this.d_reinforcements= 0;
+        this.d_assignedCountries= d_assignedCountries; 	
+       }
 
 
 }
