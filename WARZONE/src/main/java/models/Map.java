bdf790584f9202f12c9c700ac5c3f721dd2a283b
Eden Almakias <eden.almakias@gmail.com>
2024-02-13 14:45:08 -0500

    Added some comments

--- WARZONE/src/main/java/models/Map.java
+++ WARZONE/src/main/java/models/Map.java
@@ -6,16 +6,34 @@ import java.util.HashMap;
  */
 public class Map {
 
+    /**
+     * HashMap containing all countries.
+     * Key is the Country name and Value is the Country object
+     */
     private HashMap<String,Country> d_countries;
+
+    /**
+     * HashMap containing the all continents on the map.
+     * Key is the Continent name and Value is the Continent object
+     */
     private HashMap<String, Continent> d_continents;
 
+    /**
+     * Constructor that initializes the Country and Continent HashMaps
+     */
     public Map(){
         this.d_continents = new HashMap<>();
         this.d_countries = new HashMap<>();
     }
 
+    /**
+     * This method adds a country to the map. It is necessary to have an existing Continent to
+     * associate to the new country
+     * @param countryName Name of the country to be added
+     * @param continent Continent object which will be associated to new country
+     */
     public void addCountry(String countryName, Continent continent){
-        if(!this.containsContinent(continent.d_continentName)) {
+        if(!this.containsContinent(continent.d_continentName)) { //does the continent exist?
             System.out.println("Continent doesn't exist!");
             return;
         }
@@ -23,7 +41,15 @@ public class Map {
         d_countries.put(countryName, new Country(countryName, continent));
     }
 
+    /**
+     * Method which adds a border between a source and target country.
+     * This method is called from the map object but the border is created within
+     * the Country object
+     * @param source Country which will contain the new Border
+     * @param target Country which border will "point" to
+     */
     public void addBorder(String source, String target){
+        //check that both countries exist
         if(!(this.containsCountry(source) && this.containsCountry(target))){
             System.out.println("Country doesn't exist!");
             return;
@@ -33,10 +59,18 @@ public class Map {
 
     }
 
+    /**
+     * Method which add continent object ot continents HashMaps
+     * @param continent object which will be added to Map
+     */
     public void addContinent(Continent continent){
         d_continents.put(continent.d_continentName, continent);
     }
 
+    /**
+     * Method which checks if the Map is a connected graph
+     * @return true if fully connected graph, false if not
+     */
     public boolean isConnected(){
         boolean connected = true;
         for (Country c : d_countries.values()){
