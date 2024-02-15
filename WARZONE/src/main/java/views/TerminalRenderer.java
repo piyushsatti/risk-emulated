b9433d88e548caed46ed72a6a5b972f4904c5ad4
shamitad <56528168+shamitad@users.noreply.github.com>
2024-02-15 18:25:16 -0500

    Added terminal render options to TerminalRenderer in views package.

--- WARZONE/src/main/java/views/TerminalRenderer.java
+++ WARZONE/src/main/java/views/TerminalRenderer.java
@@ -1,8 +1,41 @@
 package main.java.views;
 
 public class TerminalRenderer {
-    public static void renderWelcome() {
-        StringBuilder welcome_message = new StringBuilder();
+    public static String renderWelcome() {
+        return """
+                 _    _          ______                \s
+                | |  | |        |___  /                \s
+                | |  | | __ _ _ __ / /  ___  _ __   ___\s
+                | |/\\| |/ _` | '__/ /  / _ \\| '_ \\ / _ \\\s
+                \\  /\\  / (_| | |./ /__| (_) | | | |  __/\s
+                 \\/  \\/ \\__,_|_|\\_____/\\___/|_| |_|\\___|\s
+                \s
+                Welcome to WarZone. Built by Team24.
+                """;
+    }
 
+    public static String renderMenu(String menu_type, String[] options) {
+        // Display menu graphics
+        StringBuilder out = new StringBuilder();
+        out.append(String.format("""
+                ============================
+                |  %s Options:\s
+                """, menu_type)
+        );
+        for (int i = 1; i < options.length; i++) {
+            out.append(String.format("""
+                    |     %d. %s\s
+                    """, i, options[i-1]));
+        }
+        out.append(String.format("""
+                    |     %d. Exit\s
+                    """, options.length));
+        return out.toString();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(renderWelcome());
+        String[] poop = {"Hi", "Bye", "Opti"};
+        System.out.println(renderMenu("Main Menu", poop));
     }
 }
