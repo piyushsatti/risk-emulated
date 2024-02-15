cdcd2a978b93179c9fd114eeb7f97ee57ce49eab
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-02-15 16:14:56 -0500

    Update CommandValidator.java

--- WARZONE/src/main/java/controller/CommandValidator.java
+++ WARZONE/src/main/java/controller/CommandValidator.java
@@ -1,7 +1,6 @@
-package main.java.controller;
+package controller;
 
 import exceptions.InvalidCommandException;
-import exceptions.InvalidPlayerToRemoveException;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -33,13 +32,13 @@ public class CommandValidator {
         d_validCommandList = new ArrayList<>(Arrays.asList("editcontinent", "editcountry", "editneighbor", "showmap",
                 "savemap", "editmap", "validatemap", "loadmap", "gameplayer", "deploy", "assigncountries"));
     }
-    
+
     String[] d_command_arr = null;
-    public void addCommand(String p_entered_command, String p_game_phase_string) throws InvalidCommandException, InvalidPlayerToRemoveException {
+    public void addCommand(String p_entered_command, String p_game_phase_string) throws InvalidCommandException {
         this.d_command_arr = checkCommandValidity(p_entered_command, p_game_phase_string);
     }
     public String[] checkCommandValidity(String p_enteredCommand, String p_gamePhaseString)
-            throws InvalidCommandException, NumberFormatException, InvalidPlayerToRemoveException {
+            throws InvalidCommandException {
         String[] l_command = new String[0];
         try {
             if (p_enteredCommand.isEmpty()) {
@@ -62,16 +61,15 @@ public class CommandValidator {
                     mainCommand.equals("assigncountries")) && l_command.length != 1) {
                 throw new InvalidCommandException("invalid command");
             }
-            if(mainCommand.equals("editcountry") || mainCommand.equals("editcontinent") || mainCommand.equals("editneighbor"))
-            {
+            if (mainCommand.equals("editcountry")) {
                 this.checkEditCommandsValidity(l_command);
-            }
-            else if(mainCommand.equals("gameplayer"))
-            {
+            } else if (mainCommand.equals("editcontinent")) {
+                this.checkEditCommandsValidity(l_command);
+            } else if (mainCommand.equals("editneighbor")) {
+                this.checkEditCommandsValidity(l_command);
+            } else if (mainCommand.equals("gameplayer")) {
                 this.checkGamePlayerCommandValidity(l_command);
-            }
-            else if(mainCommand.equals("deploy"))
-            {
+            } else if (mainCommand.equals("deploy")) {
                 this.checkDeployCommandValidity(l_command);
             }
             this.processValidCommand(l_command, p_gamePhaseString);
@@ -131,31 +129,114 @@ public class CommandValidator {
         }
     }
 
-    List<List<Integer>> addContinentIdList;
+    List<List<Integer>> addContinentIdContinentValList;
+    List<List<Integer>> addCountryIdContinentIdList;
+    List<List<Integer>> addCountryIdNeighborCountryId;
+    List<Integer> removeContinentIdList;
+    List<Integer> removeCountryIdList;
+    List<List<Integer>> removeCountryIdNeighborCountryIdList;
+    List<String> playersToAdd;
+    List<String> playersToRemove;
 
-    //to do..
-    private void processValidCommand(String[] p_lCommand, String p_gamePhaseString) {
-        String mainCommand = p_lCommand[0];
-        int len = p_lCommand.length;
-        if (mainCommand.equals("showmap") && p_gamePhaseString.equals("mapEditor")) {
-            //showmap(); method to show all continents, countries and their neighbors
-        } else if (mainCommand.equals("showmap") && p_gamePhaseString.equals("gamePlay")) {
-            //showmapGamePlay(); method to show all countries, continents, armies on each country, ownership, connecitvity
-        }
-        //map editor phase commands
-        if (mainCommand.equals("editcountry")) {
-            addContinentIdList = new ArrayList<>();
-            int i=1;
-            List<Integer> list = new ArrayList<>();
-            while(i<len)
-            {
-                String currOption = p_lCommand[i];
-                if(currOption.equals("-add"))
-                {
-                    list.add(Integer.valueOf(p_lCommand[i+1]));
-                    list.add(Integer.valueOf(p_lCommand[i+2]));
-                }
+    List<Integer> countryIdNumList;
+
+    private void processValidCommand(String[] p_lCommand, String p_gamePhaseString) throws NumberFormatException  {
+        try {
+            String mainCommand = p_lCommand[0];
+            int len = p_lCommand.length;
+            if(mainCommand.equals("showmap") && p_gamePhaseString.equals("mapEditor")) {
+                //showmap(); method to show all continents, countries and their neighbors
+            } else if (mainCommand.equals("showmap") && p_gamePhaseString.equals("gamePlay")) {
+                //showmapGamePlay(); method to show all countries, continents, armies on each country, ownership, connecitvity
             }
+            if (mainCommand.equals("editcontinent")) {
+                addContinentIdContinentValList = new ArrayList<>();
+                removeContinentIdList = new ArrayList<>();
+                int i = 1;
+                while (i < len) {
+                    String currOption = p_lCommand[i];
+                    if (currOption.equals("-add")) {
+                        int addContinentId = Integer.parseInt(p_lCommand[++i]);
+                        int addContinentVal = Integer.parseInt(p_lCommand[++i]);
+                        List<Integer> pair = new ArrayList<>();
+                        pair.add(addContinentId);
+                        pair.add(addContinentVal);
+                        addContinentIdContinentValList.add(pair);
+                    } else if (currOption.equals("remove")) {
+                        int removeContinentId = Integer.parseInt(p_lCommand[++i]);
+                        removeContinentIdList.add(removeContinentId);
+                    }
+                }
+                //some method to process the command with data saved in the lists
+            } else if (mainCommand.equals("editcountry")) {
+                addCountryIdContinentIdList = new ArrayList<>();
+                removeCountryIdList = new ArrayList<>();
+                int j = 1;
+                while (j < len) {
+                    String currOption = p_lCommand[j];
+                    if (currOption.equals("-add")) {
+                        int addCountryId = Integer.parseInt(p_lCommand[++j]);
+                        int addContinentId = Integer.parseInt(p_lCommand[++j]);
+                        List<Integer> pair = new ArrayList<>();
+                        pair.add(addCountryId);
+                        pair.add(addContinentId);
+                        addCountryIdContinentIdList.add(pair);
+                    } else if (currOption.equals("remove")) {
+                        int removeCountryId = Integer.parseInt(p_lCommand[++j]);
+                        removeCountryIdList.add(removeCountryId);
+                    }
+                }
+                //some method to process the command with data saved in the lists
+            } else if (mainCommand.equals("editneighbor")) {
+                addCountryIdNeighborCountryId = new ArrayList<>();
+                removeCountryIdNeighborCountryIdList = new ArrayList<>();
+                int k = 1;
+                while (k < len) {
+                    String currOption = p_lCommand[k];
+                    if (currOption.equals("-add")) {
+                        int addCountryId = Integer.parseInt(p_lCommand[++k]);
+                        int addNeighborCountryId = Integer.parseInt(p_lCommand[++k]);
+                        List<Integer> pair = new ArrayList<>();
+                        pair.add(addCountryId);
+                        pair.add(addNeighborCountryId);
+                        addCountryIdContinentIdList.add(pair);
+                    } else if (currOption.equals("remove")) {
+                        List<Integer> pair = new ArrayList<>();
+                        int removeCountryId = Integer.parseInt(p_lCommand[++k]);
+                        int removeNeighborCountryId = Integer.parseInt(p_lCommand[++k]);
+                        removeCountryIdNeighborCountryIdList.add(pair);
+                    }
+                }
+                //some method to process the command with data saved in the lists
+            } else if (mainCommand.equals("gameplayer")) {
+                playersToAdd = new ArrayList<>();
+                playersToRemove = new ArrayList<>();
+                int z = 1;
+                while (z < len) {
+                    String currOption = p_lCommand[z];
+                    if (currOption.equals("-add")) {
+                        String playerToAdd = p_lCommand[++z];
+                        playersToAdd.add(playerToAdd);
+                    } else if (currOption.equals("-remove")) {
+                        String playerToRemove = p_lCommand[++z];
+                        playersToRemove.add(playerToRemove);
+                    }
+                }
+                //some method to process the command with data saved in the lists
+            } else if (mainCommand.equals("deploy")) {
+                int countryId = Integer.parseInt(p_lCommand[1]);
+                int numReinforcements = Integer.parseInt(p_lCommand[2]);
+                countryIdNumList.add(countryId);
+                countryIdNumList.add(numReinforcements);
+            } else if (mainCommand.equals("savemap")) {//method to savemap
+            } else if (mainCommand.equals("editmap")) {//method to edit map
+            } else if (mainCommand.equals("validatemap")) {//method to validate map
+            } else if (mainCommand.equals("loadmap")) {//method to load map
+            } else if (mainCommand.equals("assigncountries")) {//method to assign countries
+            }
+        }
+        catch(NumberFormatException e) {
+            System.out.println(e.getMessage());
         }
     }
 
