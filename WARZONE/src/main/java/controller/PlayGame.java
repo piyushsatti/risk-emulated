8a86ea32716c47bddb2512b8528fe8e507f5e472
Devdutt Sharma <64033478+devdutt13@users.noreply.github.com>
2024-02-17 12:28:32 +0530

    Added methods assignReinforcements, playerOrders, executeOrders in PlayGame

--- WARZONE/src/main/java/controller/PlayGame.java
+++ WARZONE/src/main/java/controller/PlayGame.java
@@ -14,13 +14,12 @@ public class PlayGame {
 
     public static void startGame() throws FileNotFoundException {
 
-
         //We need list of players here
         ArrayList<Player> l_listOfPlayers = GameEngine.PLAYER_LIST;
         System.out.println("Assign countries input from user");
+        //NOTE TO PIYUSH: Since assigncountries is a command we need a view for it so user can enter it.
         //Assigning Countries
          assignCountriesToPlayers(l_listOfPlayers);
-
          //View to show that the startup phase is done - View StartUp.
 
         gameLoop(l_listOfPlayers);
@@ -28,7 +27,7 @@ public class PlayGame {
 
     }
 
-    public static void assignCountriesToPlayers(ArrayList<Player> l_listOfPlayers) throws FileNotFoundException {
+    public static void assignCountriesToPlayers(ArrayList<Player> p_listOfPlayers) throws FileNotFoundException {
         WorldMap map = GameEngine.CURRENT_MAP; //Take map from game engine
 
 
@@ -59,30 +58,68 @@ public class PlayGame {
 
 
        HashMap<Integer, Country> listOfCountries = map.getCountries();
-        int total_players = l_listOfPlayers.size();
+        int total_players = p_listOfPlayers.size();
         int playerNumber =0;
         for(HashMap.Entry<Integer, main.java.models.worldmap.Country> entry : listOfCountries.entrySet()) {
             if ((playerNumber % total_players == 0) && playerNumber != 0) {
                 playerNumber =0;
             }
             Integer key = entry.getKey();
-            Country value = entry.getValue();
-            Player p = l_listOfPlayers.get(playerNumber);
-            p.setAssignedCountries(key);
-
+            p_listOfPlayers.get(playerNumber).setAssignedCountries(key);
             playerNumber++;
         }
 
-        for(Player l_player: l_listOfPlayers){
-            //l_player.printPlayerDetails();
-            System.out.println("Assigning Done");
-        }
-        for(Player l_player: l_listOfPlayers){
+        System.out.println("Assigning of Countries Done");
+        for(Player l_player: p_listOfPlayers){
             System.out.println("Number of Countries: " + l_player.getAssignedCountries().size());
             System.out.println(" ");
         }
 
     }
+    public static void assignReinforcements(ArrayList<Player> p_listOfPlayers){
+        System.out.println("Assigning Reinforcements");
+        for(Player player : p_listOfPlayers){
+            int l_numberOfTroops = Math.max((int) player.getAssignedCountries().size() / 3, 3);
+            player.setReinforcements(l_numberOfTroops);
+        }
+        System.out.println("Reinforcements Assigned");
+
+    }
+    public static void playerOrders(ArrayList<Player> p_listOfPlayers){
+        System.out.println("Please Start issuing orders");
+        int l_totalplayers = p_listOfPlayers.size();
+        int l_playerNumber =0;
+        while(!allTroopsPlaced(p_listOfPlayers)){
+            if((l_playerNumber % l_totalplayers == 0) && l_playerNumber!=0){
+                l_playerNumber =0;}
+            if(p_listOfPlayers.get(l_playerNumber).getReinforcements()!=0){
+                //Issue Order View  -- To which country do you want to deploy and how much
+                p_listOfPlayers.get(l_playerNumber).issue_order(1,3);}
+
+            l_playerNumber++;
+        }
+
+    }
+    public static void executingOrders(ArrayList<Player> p_listOfPlayers){
+        System.out.println("Executing Orders");
+        int l_totalplayers = p_listOfPlayers.size();
+        int l_playerNumber =0;
+
+        while(!allOrdersExecuted(p_listOfPlayers)){
+            if((l_playerNumber % l_totalplayers == 0) && l_playerNumber!=0){
+                l_playerNumber =0;
+            }
+            if (!p_listOfPlayers.get(l_playerNumber).getOrderList().isEmpty()) {
+                Order order = p_listOfPlayers.get(l_playerNumber).next_order();
+                order.execute();
+            }
+            l_playerNumber++;
+        }
+        System.out.println("All Orders Executed Successfully");
+
+    }
+
+
 
     //Checking if player has placed all his troops.
     public static boolean allTroopsPlaced(ArrayList<Player> p_Players){
@@ -105,46 +142,14 @@ public class PlayGame {
         return true;
     }
 
+    public static void gameLoop(ArrayList<Player> p_listOfPlayers ) {
+
+        assignReinforcements(p_listOfPlayers);
+        playerOrders(p_listOfPlayers);
+        allOrdersExecuted(p_listOfPlayers);
 
 
-    public static void gameLoop(ArrayList<Player> l_listOfPlayers ) {
-
-        System.out.println("Assigning Reinforcements");
-        for(Player player : l_listOfPlayers){
-            int l_numberOfTroops = Math.max((int) player.getAssignedCountries().size() / 3, 3);
-            player.setReinforcements(l_numberOfTroops);
-        }
-        System.out.println("Reinforcements Assigned");
-
-        System.out.println("Please Start issuing orders");
-
-
-        int l_totalplayers = l_listOfPlayers.size();
-        int l_playerNumber =0;
-        while(!allTroopsPlaced(l_listOfPlayers)){
-            if((l_playerNumber % l_totalplayers == 0) && l_playerNumber!=0){
-                l_playerNumber =0;}
-            if(l_listOfPlayers.get(l_playerNumber).getReinforcements()!=0){
-                //Issue Order View  -- To which country do you want to deploy and how much
-                l_listOfPlayers.get(l_playerNumber).issue_order(1,3);}
-
-            l_playerNumber++;
-        }
-
-
-        //Execute order for all players
-        System.out.println("Executing Orders");
-        while(!allOrdersExecuted(l_listOfPlayers)){
-            if((l_playerNumber % l_totalplayers == 0) && l_playerNumber!=0){
-                l_playerNumber =0;
-            }
-            if (!l_listOfPlayers.get(l_playerNumber).getOrderList().isEmpty()) {
-                Order order = l_listOfPlayers.get(l_playerNumber).next_order();
-                order.execute();
-            }
-            l_playerNumber++;
-        }
-        System.out.println("Orders Executed");
+        System.out.println("Game Loop Completed");
 
 
     }
