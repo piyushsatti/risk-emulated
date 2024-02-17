1c4f235b75fc1168f1a5303c5ea9e419e124972c
Eden Almakias <eden.almakias@gmail.com>
2024-02-17 15:50:05 -0500

    Added messaging to new exception classes to handle both int ID and String name

--- WARZONE/src/main/java/utils/exceptions/DuplicateContinentException.java
+++ WARZONE/src/main/java/utils/exceptions/DuplicateContinentException.java
@@ -1,4 +1,10 @@
 package main.java.utils.exceptions;
 
 public class DuplicateContinentException extends Exception{
+    public DuplicateContinentException(int id){
+        super("Continent " + id + " already exists");
+    }
+    public DuplicateContinentException(String name){
+        super("Continent " + name + " already exists");
+    }
 }
