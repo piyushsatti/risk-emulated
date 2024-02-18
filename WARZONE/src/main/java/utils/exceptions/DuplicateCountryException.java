c2262a494245ff83cb0cb0fc2efa60122ecb60ef
shamitad <shamita.datta119@gmail.com>
2024-02-18 07:44:14 -0500

    added javadoc to DuplicateCountryException

--- WARZONE/src/main/java/utils/exceptions/DuplicateCountryException.java
+++ WARZONE/src/main/java/utils/exceptions/DuplicateCountryException.java
@@ -1,9 +1,25 @@
 package main.java.utils.exceptions;
 
+/**
+ * The DuplicateCountryException class represents an exception that is thrown
+ * when attempting to add a duplicate country.
+ */
 public class DuplicateCountryException extends Exception{
+    /**
+     * Constructs a DuplicateCountryException with a detail message indicating
+     * that a country with the specified ID already exists.
+     *
+     * @param id The ID of the country that already exists.
+     */
     public DuplicateCountryException(int id){
         super("Country " + id + " already exists");
     }
+    /**
+     * Constructs a DuplicateCountryException with a detail message indicating
+     * that a country with the specified name already exists.
+     *
+     * @param name The name of the country that already exists.
+     */
     public DuplicateCountryException(String name){
         super("Country " + name + " already exists");
     }
