da5ed712df385654f2205d8ca2c18c168d0f9fb1
Eden Almakias <eden.almakias@gmail.com>
2024-02-18 16:10:50 -0500

    Coding conventions editing in country class (adding l_ to local vars and p_ to functions params) Removed unused addBorders method Changed return value of country/continent not found to -1 from 0

--- WARZONE/src/main/java/models/worldmap/WorldMap.java
+++ WARZONE/src/main/java/models/worldmap/WorldMap.java
@@ -54,30 +54,30 @@ public class WorldMap {
     /**
      * Adds a country to the world map.
      *
-     * @param countryID   The ID of the country to be added.
-     * @param continentID The ID of the continent to which the country belongs.
-     * @param countryName The name of the country to be added.
+     * @param p_countryID   The ID of the country to be added.
+     * @param p_continentID The ID of the continent to which the country belongs.
+     * @param p_countryName The name of the country to be added.
      */
-    public void addCountry(int countryID, int continentID, String countryName){
+    public void addCountry(int p_countryID, int p_continentID, String p_countryName){
 
-        if (!this.containsContinent(continentID)) { //does the continent exist?
-
-            //throw exception
-            return;
-
-        } else if (this.containsCountry(countryID)) { //duplicate country id
+        if (!this.containsContinent(p_continentID)) { //does the continent exist?
 
             //throw exception
             return;
 
-        } else if (this.containsCountry(countryName)) { //duplicate country name
+        } else if (this.containsCountry(p_countryID)) { //duplicate country id
+
+            //throw exception
+            return;
+
+        } else if (this.containsCountry(p_countryName)) { //duplicate country name
 
             //throw exception
             return;
 
         }
 
-        d_countries.put(countryID, new Country(countryID, countryName, d_continents.get(continentID)));
+        d_countries.put(p_countryID, new Country(p_countryID, p_countryName, d_continents.get(p_continentID)));
 
     }
 
@@ -93,18 +93,18 @@ public class WorldMap {
      * This method is called from the map object but the border is created within
      * the Country object
      *
-     * @param source Country which will contain the new Border
-     * @param target Country which border will "point" to
+     * @param p_source Country which will contain the new Border
+     * @param p_target Country which border will "point" to
      */
-    public void addBorder(int source, int target) {
+    public void addBorder(int p_source, int p_target) {
 
         //check that both countries exist
-        if (!this.containsCountry(source)) {
+        if (!this.containsCountry(p_source)) {
 
             //throw exception
             return;
 
-        } else if (!this.containsCountry(target)) {
+        } else if (!this.containsCountry(p_target)) {
 
             //throw exception
 
@@ -112,57 +112,33 @@ public class WorldMap {
 
         }
 
-        this.getCountry(source).addBorder(this.getCountry(target));
+        this.getCountry(p_source).addBorder(this.getCountry(p_target));
 
     }
 
-    /**
-     * Adds borders between the source country and a list of target countries.
-     *
-     * @param source     The ID of the source country.
-     * @param borderList The list of IDs of target countries to add borders with.
-     */
-    public void addBorders(int source, ArrayList<Integer> borderList){
-
-        //check that both countries exist
-        for (Integer i : borderList) {
-
-            if (!this.containsCountry(i)) {
-
-                //throw exception
-                return;
-
-            }
-
-        }
-
-        for (Integer i : borderList) {
-            this.addBorder(source, i);
-        }
-
-    }
 
     /**
      * Method which adds continent to map
      *
-     * @param id new continent identifier
-     * @param continentName new continent name
+     * @param p_id new continent identifier
+     * @param p_continentName new continent name
+     * @param p_bonus bonus value for new continent
      */
-    public void addContinent(int id, String continentName, int bonus) {
+    public void addContinent(int p_id, String p_continentName, int p_bonus) {
 
-        if (this.containsContinent(id)) { //duplicate continent
+        if (this.containsContinent(p_id)) { //duplicate continent
 
             //throw error
             return;
 
-        } else if (this.containsContinent(continentName)) { //duplicate name
+        } else if (this.containsContinent(p_continentName)) { //duplicate name
 
             //throw error
             return;
 
         }
 
-        d_continents.put(id, new Continent(id, continentName, bonus));
+        d_continents.put(p_id, new Continent(p_id, p_continentName, p_bonus));
 
     }
 
@@ -172,31 +148,31 @@ public class WorldMap {
      */
     public boolean isConnected() {
 
-        boolean connected = true;
+        boolean l_connected = true;
 
-        for (Country c : d_countries.values()) {
-            connected = connected && c.canReach(d_countries);
+        for (Country l_c : d_countries.values()) {
+            l_connected = l_connected && l_c.canReach(d_countries);
         }
 
-        return connected;
+        return l_connected;
 
     }
 
     /**
      * Checks if the countries in the provided list are all connected.
      *
-     * @param countryList The list of countries to check for connectivity.
+     * @param p_countryList The hashmap of countries to check for connectivity.
      * @return true if all countries in the list are connected, false otherwise.
      */
-    public boolean isConnected(HashMap<Integer, Country> countryList) {
+    public boolean isConnected(HashMap<Integer, Country> p_countryList) {
 
-        boolean connected = true;
+        boolean l_connected = true;
 
-        for (Country c : countryList.values()) {
-            connected = connected && c.canReach(countryList);
+        for (Country l_c : p_countryList.values()) {
+            l_connected = l_connected && l_c.canReach(p_countryList);
         }
 
-        return connected;
+        return l_connected;
 
     }
 
@@ -207,196 +183,196 @@ public class WorldMap {
      */
     public boolean isContinentConnected() {
 
-        boolean connected = true;
+        boolean l_connected = true;
 
-        for (Continent target : this.d_continents.values()) {
+        for (Continent l_target : this.d_continents.values()) {
 
-            HashMap<Integer,Country> tempCont = this.getContinentCountries(target);
+            HashMap<Integer,Country> l_tempCont = this.getContinentCountries(l_target);
 
-            connected = connected && isConnected(tempCont);
+            l_connected = l_connected && isConnected(l_tempCont);
 
         }
 
-        return connected;
+        return l_connected;
 
     }
 
     /**
      * Method which gets list of countries associated with the provided continent
      *
-     * @param continent Continent object for which countries should be retrieved
+     * @param p_continent Continent object for which countries should be retrieved
      * @return HashMap of countries associated with continent
      */
-    public HashMap<Integer, Country> getContinentCountries(Continent continent) {
+    public HashMap<Integer, Country> getContinentCountries(Continent p_continent) {
 
-        HashMap<Integer, Country> output = new HashMap<>();
+        HashMap<Integer, Country> l_output = new HashMap<>();
 
-        for (Country c : this.d_countries.values()) {
+        for (Country l_c : this.d_countries.values()) {
 
-            if (c.getContinent().equals(continent)) {
+            if (l_c.getContinent().equals(p_continent)) {
 
-                output.put(c.getCountryID(), c);
+                l_output.put(l_c.getCountryID(), l_c);
 
             }
 
         }
 
-        return output;
+        return l_output;
 
     }
 
     /**
      * Method to remove a continent from the map.
      *
-     * @param continentID The identifier of the continent to be removed.
+     * @param p_continentID The identifier of the continent to be removed.
      */
-    public void removeContinent(int continentID) {
+    public void removeContinent(int p_continentID) {
 
-        Continent continent = d_continents.get(continentID);
+        Continent l_continent = d_continents.get(p_continentID);
 
-        for (Country country : getContinentCountries(continent).values()) {
+        for (Country l_country : getContinentCountries(l_continent).values()) {
 
-            this.removeCountry(country.getCountryID());
+            this.removeCountry(l_country.getCountryID());
 
         }
 
-        d_continents.remove(continentID);
+        d_continents.remove(p_continentID);
 
     }
 
     /**
      * This method removes a country from the map.
      *
-     * @param countryID The unique identifier of the country to be removed.
+     * @param p_countryID The unique identifier of the country to be removed.
      */
-    public void removeCountry(int countryID) {
+    public void removeCountry(int p_countryID) {
 
-        Country country = d_countries.get(countryID);
+        Country l_country = d_countries.get(p_countryID);
 
-        for (Country countryToCheck : this.d_countries.values()) {
+        for (Country l_countryToCheck : this.d_countries.values()) {
 
-            countryToCheck.removeBorder(country);
+            l_countryToCheck.removeBorder(l_country);
 
         }
 
-        d_countries.remove(countryID);
+        d_countries.remove(p_countryID);
 
     }
 
     /**
      * Method that checks if country exists in map
      *
-     * @param countryID Country identifier integer
+     * @param p_countryID Country identifier integer
      * @return true if found false if not found
      */
-    public boolean containsCountry(int countryID){
-        return this.d_countries.containsKey(countryID);
+    public boolean containsCountry(int p_countryID){
+        return this.d_countries.containsKey(p_countryID);
     }
 
     /**
      * Checks if the world map contains a country with the specified name.
      *
-     * @param countryName The name of the country to check.
+     * @param p_countryName The name of the country to check.
      * @return true if the world map contains a country with the specified name, false otherwise.
      */
-    public boolean containsCountry(String countryName){
-        return this.containsCountry(this.getCountryID(countryName));
+    public boolean containsCountry(String p_countryName){
+        return this.containsCountry(this.getCountryID(p_countryName));
     }
 
     /**
      * Method that checks if continent exists in map
      *
-     * @param continentID Continent identifier integer
+     * @param p_continentID Continent identifier integer
      * @return true if found false if not found
      */
-    public boolean containsContinent(int continentID){
-        return this.d_continents.containsKey(continentID);
+    public boolean containsContinent(int p_continentID){
+        return this.d_continents.containsKey(p_continentID);
     }
 
     /**
      * Checks if the world map contains a continent with the specified name.
      *
-     * @param continentName The name of the continent to check.
+     * @param p_continentName The name of the continent to check.
      * @return true if the world map contains a continent with the specified name, false otherwise.
      */
-    public boolean containsContinent(String continentName){
+    public boolean containsContinent(String p_continentName){
 
-        return this.containsContinent(this.getContinentID(continentName));
+        return this.containsContinent(this.getContinentID(p_continentName));
 
     }
 
     /**
      * Retrieve country object based on country ID
      *
-     * @param countryID Identifier of country to search for
+     * @param p_countryID Identifier of country to search for
      * @return Country object with matching identifier
      */
-    public Country getCountry(int countryID){
-        return this.d_countries.get(countryID);
+    public Country getCountry(int p_countryID){
+        return this.d_countries.get(p_countryID);
     }
 
     /**
      * Removes a border between two countries.
      *
-     * @param source The ID of the source country.
-     * @param target The ID of the target country.
+     * @param p_source The ID of the source country.
+     * @param p_target The ID of the target country.
      * @throws IllegalArgumentException if either the source or target country does not exist.
      */
-    public void removeBorder(int source, int target){
+    public void removeBorder(int p_source, int p_target){
 
         //check that both countries exist
-        if (!(this.containsCountry(source) && this.containsCountry(target))) {
+        if (!(this.containsCountry(p_source) && this.containsCountry(p_target))) {
 
             //throw exception
             return;
 
         }
 
-        this.getCountry(source).removeBorder(this.getCountry(target));
+        this.getCountry(p_source).removeBorder(this.getCountry(p_target));
 
     }
 
     /**
      * Gets the ID of a country based on its name.
      *
-     * @param countryName The name of the country.
-     * @return The ID of the country, or 0 if not found.
+     * @param p_countryName The name of the country.
+     * @return The ID of the country, or -1 if not found.
      */
-    public int getCountryID(String countryName){
+    public int getCountryID(String p_countryName){
 
-        for (Country c : this.getCountries().values()){
+        for (Country l_c : this.getCountries().values()){
 
-            if (c.getCountryName().equals(countryName)) {
+            if (l_c.getCountryName().equals(p_countryName)) {
 
-                return c.getCountryID();
+                return l_c.getCountryID();
 
             }
 
         }
 
-        return 0;
+        return -1;
 
     }
 
     /**
      * Gets the ID of a continent based on its name.
      *
-     * @param continentName The name of the continent.
-     * @return The ID of the continent, or 0 if not found.
+     * @param p_continentName The name of the continent.
+     * @return The ID of the continent, or -1 if not found.
      */
-    public int getContinentID(String continentName){
+    public int getContinentID(String p_continentName){
 
-        for (Continent c : this.d_continents.values()) {
+        for (Continent l_c : this.d_continents.values()) {
 
-            if (c.getContinentName().equals(continentName)) {
+            if (l_c.getContinentName().equals(p_continentName)) {
 
-                return c.getContinentID();
+                return l_c.getContinentID();
 
             }
 
         }
 
-        return 0;
+        return -1;
 
     }
 
