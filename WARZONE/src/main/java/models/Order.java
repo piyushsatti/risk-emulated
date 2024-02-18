fe73dc970b5a733866a7277f4390c8477fbfe7fb
shamitad <shamita.datta119@gmail.com>
2024-02-18 07:05:11 -0500

    Order with javadoc

--- WARZONE/src/main/java/models/Order.java
+++ WARZONE/src/main/java/models/Order.java
@@ -2,8 +2,10 @@ package main.java.models;
 
 import main.java.controller.GameEngine;
 
-
-
+/**
+ * The Order class represents a player's order in the game.
+ * It specifies the movement of reinforcements from one country to another.
+ */
 public class Order {
     int d_playerOrderID;
     String d_playerOrderName;
@@ -11,6 +13,14 @@ public class Order {
     private final int d_toCountryID;
     private final int d_reinforcementsDeployed;
 
+    /**
+     * Constructs an Order object with specified attributes.
+     *
+     * @param p_playerOrderName The name of the player issuing the order.
+     * @param p_playerOrderID The ID of the player issuing the order.
+     * @param p_fromCountryID The ID of the country from which reinforcements are deployed.
+     * @param p_reinforcementsDeployed The number of reinforcements deployed.
+     */
     public Order(String p_playerOrderName, int p_playerOrderID,int p_fromCountryID,int p_reinforcementsDeployed){
         this.d_playerOrderName = p_playerOrderName;
         this.d_playerOrderID = p_playerOrderID;
@@ -19,12 +29,14 @@ public class Order {
         this.d_reinforcementsDeployed = p_reinforcementsDeployed;
     }
 
+    /**
+     * Executes the order by deploying reinforcements to the specified country.
+     */
     public void execute(){
         int l_currentReinforcements = GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).getReinforcements();
         GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).setReinforcements(this.d_reinforcementsDeployed + l_currentReinforcements);
         System.out.print("Order Executed: ");
         System.out.println(this.d_reinforcementsDeployed +" troops deployed on "+ GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).getD_countryName() + " by " +this.d_playerOrderName);
-        
 
     }
 }
