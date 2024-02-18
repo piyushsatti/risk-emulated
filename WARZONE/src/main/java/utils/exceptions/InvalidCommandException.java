c9a82f3d3ceaa494d12daa8619dd070928a89c8a
shamitad <shamita.datta119@gmail.com>
2024-02-18 07:40:22 -0500

    added javadoc to InvalidCommandExceptionexception

--- WARZONE/src/main/java/utils/exceptions/InvalidCommandException.java
+++ WARZONE/src/main/java/utils/exceptions/InvalidCommandException.java
@@ -1,6 +1,16 @@
-package exceptions;
+package main.java.utils.exceptions;
 
+
+/**
+ * The InvalidCommandException class represents an exception that is thrown
+ * when an invalid command is encountered.
+ */
 public class InvalidCommandException extends Exception {
+    /**
+     * Constructs an InvalidCommandException with the specified detail message.
+     *
+     * @param message The detail message explaining the reason for the exception.
+     */
     public InvalidCommandException(String message) {
         super(message);
     }
