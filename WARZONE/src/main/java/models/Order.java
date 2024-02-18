e08e7c51cd1e2fb449aadaecdafe7ab1b0f8fb21
Devdutt Sharma <64033478+devdutt13@users.noreply.github.com>
2024-02-18 03:45:56 -0500

    Update Order.java

--- WARZONE/src/main/java/models/Order.java
+++ WARZONE/src/main/java/models/Order.java
@@ -6,26 +6,25 @@ import main.java.controller.GameEngine;
 
 public class Order {
     int d_playerOrderID;
-
+    String d_playerOrderName;
     private final int d_fromCountryID;
     private final int d_toCountryID;
     private final int d_reinforcementsDeployed;
 
-    public Order(int p_playerOrderID,int p_fromCountryID,int p_reinforcementsDeployed){
+    public Order(String p_playerOrderName, int p_playerOrderID,int p_fromCountryID,int p_reinforcementsDeployed){
+        this.d_playerOrderName = p_playerOrderName;
         this.d_playerOrderID = p_playerOrderID;
         this.d_fromCountryID = p_fromCountryID;
         this.d_toCountryID = -1;
         this.d_reinforcementsDeployed = p_reinforcementsDeployed;
     }
-//    public void printOrder(){
-//        System.out.println("Name: " + this.d_PlayerName + " Order Type: " + this.d_orderType + " Country ID: " + this.d_countryID + "Troops to Be Deployed: " + this.d_deployedTroops);
-//    }
-//    public void printExecutedOrder(Country p_selectedCountry){
-//        System.out.println("Name: " + this.d_PlayerName + " Order Type: " + this.d_orderType + " Country ID: " + this.d_countryID + "Troops Deployed on Country: " + p_selectedCountry.getD_deployedReinforcements());
-//    }
+
     public void execute(){
-        GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).setReinforcements(this.d_reinforcementsDeployed);
-        System.out.println("Order Executed");
+        int l_currentReinforcements = GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).getReinforcements();
+
+        GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).setReinforcements(this.d_reinforcementsDeployed + l_currentReinforcements);
+        System.out.print("Order Executed: ");
+        System.out.println(this.d_reinforcementsDeployed +" troops deployed on "+ GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).getD_countryName() + " by " +this.d_playerOrderName);
         
 
     }
