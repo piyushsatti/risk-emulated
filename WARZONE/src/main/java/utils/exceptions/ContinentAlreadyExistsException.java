f716b8f7d62be5f7b7e34d75e62b1059cf33acd7
shamitad <shamita.datta119@gmail.com>
2024-02-18 07:41:12 -0500

    added javadoc to ContinentAlreadyExistsException

--- WARZONE/src/main/java/utils/exceptions/ContinentAlreadyExistsException.java
+++ WARZONE/src/main/java/utils/exceptions/ContinentAlreadyExistsException.java
@@ -1,6 +1,17 @@
 package main.java.utils.exceptions;
 
+/**
+ * The ContinentAlreadyExistsException class represents an exception that is thrown
+ * when attempting to add a continent that already exists in the game.
+ */
 public class ContinentAlreadyExistsException extends Exception {
+
+    /**
+     * Constructs a ContinentAlreadyExistsException with a detail message indicating
+     * that the continent with the specified name already exists.
+     *
+     * @param name The name of the continent that already exists.
+     */
     public ContinentAlreadyExistsException(String name){
         super("Continent " +name+ " already exists, cannot add it again ");
     }
