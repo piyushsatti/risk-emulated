af25cd22f12b27fe9a95992eea28dd1cddb48ab0
shamitad <shamita.datta119@gmail.com>
2024-02-18 07:45:00 -0500

    added javadoc to PlayerDoesNotExistException

--- WARZONE/src/main/java/utils/exceptions/PlayerDoesNotExistException.java
+++ WARZONE/src/main/java/utils/exceptions/PlayerDoesNotExistException.java
@@ -1,6 +1,16 @@
 package main.java.utils.exceptions;
 
+/**
+ * The PlayerDoesNotExistException class represents an exception that is thrown
+ * when attempting to access a player that does not exist in the game.
+ */
 public class PlayerDoesNotExistException extends Exception {
+    /**
+     * Constructs a PlayerDoesNotExistException with a detail message indicating
+     * that the player with the specified name does not exist.
+     *
+     * @param p_playerName The name of the player that does not exist.
+     */
     public PlayerDoesNotExistException(String p_playerName){
         super("player does not exist: " + p_playerName);
     }
