abf6fdd47c3c6cd6ea9deb47aac5eb00c5a14f75
shamitad <shamita.datta119@gmail.com>
2024-02-18 22:32:35 -0500

    javadoc updated for MapInterfaceTest

--- WARZONE/src/test/java/controller/MapInterfaceTest.java
+++ WARZONE/src/test/java/controller/MapInterfaceTest.java
@@ -2,9 +2,16 @@ package controller;
 
 import org.junit.Test;
 
+/**
+ * The MapInterfaceTest class contains unit tests for the MapInterface class.
+ * It includes test methods to validate map-related functionalities.
+ */
 public class MapInterfaceTest {
 
-
+    /**
+     * Tests the validateMap method of the MapInterface class.
+     * This method checks if the map validation functionality behaves as expected.
+     */
     @Test
     public void validateMap() {
     }
