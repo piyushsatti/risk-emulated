1bb93370aafc46f4567c033579f7df857ea5306b
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-03-15 18:08:05 -0400

    Update MainMenuCommands.java

--- WARZONE/src/main/java/controller/middleware/commands/MainMenuCommands.java
+++ WARZONE/src/main/java/controller/middleware/commands/MainMenuCommands.java
@@ -13,15 +13,7 @@ public class MainMenuCommands extends Commands {
     @Override
     public boolean validateCommand() {
         Pattern pattern = Pattern.compile(
-                "^loadmap\\s\\w+\\.map$|" +
-                        "^assigncountries$|" +
-                        "^gameplayer(?:\\s+-add\\s+\\w+)*(?:\\s+-remove\\s+\\w+)*$|" +
-                        "^deploy\\s+\\w+\\s+\\d+$|" +
-                        "^advance\\s+\\w+\\s+\\w+\\s+\\d+$|" +
-                        "^bomb\\s+\\w+\\s*$|" +
-                        "^blockade\\s+\\w+\\s*$|" +
-                        "^negotiate\\s+\\w+\\s*$|" +
-                        "^airlift\\s+\\w+\\s+\\w+\\s+\\d+$"
+                "^loadmap\\s\\w+\\.map$"
         );
         Matcher matcher = pattern.matcher(d_command);
         return matcher.matches();
