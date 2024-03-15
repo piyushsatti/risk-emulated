74800d4eedcd4110ea52195157823351d6af2b59
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-03-15 16:50:53 -0400

    Create GamePlayCommandsTest.java

---
+++ WARZONE/src/main/java/controller/middleware/commands/GamePlayCommandsTest.java
@@ -0,0 +1,136 @@
+package controller.middleware.commands;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GamePlayCommandsTest {
+    @Test
+    public void testLoadMap1()
+    {
+        String cmd = "loadmap ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testLoadMap2()
+    {
+        String cmd = "loadmap piyush";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testLoadMap3()
+    {
+        String cmd = "loadmap piyush.map";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertTrue(obj.validateCommand());
+    }
+    @Test
+    public void testLoadMap4()
+    {
+        String cmd = "loadmap piyush satti.map";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+
+    @Test
+    public void testGamePlayer1()
+    {
+        String cmd = "gameplayer ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer2()
+    {
+        String cmd = "gameplayer Shashi Piyush";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer3()
+    {
+        String cmd = "gameplayer -add Shashi -remove Piyush";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertTrue(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer4()
+    {
+        String cmd = "gameplayer -add Shashi";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertTrue(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer5()
+    {
+        String cmd = "gameplayer -remove Shashi";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertTrue(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer6()
+    {
+        String cmd = "gameplayer -add -remove Shashi ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer7()
+    {
+        String cmd = "gameplayer -add Shashi -remove ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer8()
+    {
+        String cmd ="gameplayer -add Shashi -remove ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer9()
+    {
+        String cmd ="gameplayer -add Shashi Piyush Devdutt -remove Priyanshu";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer10()
+    {
+        String cmd ="gameplayer -add Shashi -remove Priyanshu Piyush Devdutt";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testGamePlayer11()
+    {
+        String cmd ="gameplayer -add Shashi -remove Priyanshu -remove Piyush -add Devdutt";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertTrue(obj.validateCommand());
+    }
+
+    @Test
+    public void testAssignCountries1()
+    {
+        String cmd = "assigncountries India ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testAssignCountries2()
+    {
+        String cmd = "assigncountries India Nepal ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertFalse(obj.validateCommand());
+    }
+    @Test
+    public void testAssignCountries3()
+    {
+        String cmd = "assigncountries ";
+        MapEditorCommands obj = new MapEditorCommands(cmd);
+        assertTrue(obj.validateCommand());
+    }
+}
