c8466a2c63e62b75b7f428b15e1426cb05e44b04
Eden Almakias <eden.almakias@gmail.com>
2024-03-15 14:21:52 -0400

    State pattern classes and sequence

--- WARZONE/src/main/java/controller/commands/MapEditorCommand.java
+++
@@ -1,4 +0,0 @@
-package controller.commands;
-
-public class MapEditorCommand {
-}
---
+++ WARZONE/src/main/java/controller/states/OrderIssuing.java
@@ -0,0 +1,34 @@
+package controller.states;
+
+import controller.GameEngine;
+
+public class OrderIssuing extends State {
+    public OrderIssuing(GameEngine g) {
+        super(g);
+    }
+
+    @Override
+    public void displayMenu() {
+
+    }
+
+    @Override
+    public void next() {
+
+    }
+
+    @Override
+    public void endGame() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+    @Override
+    public void processInput() {
+
+    }
+}
