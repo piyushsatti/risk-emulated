9204ebdfee43a7a7d9daa23861b9d384cb320a40
piyushsatti <piyushsatti@gmail.com>
2024-03-15 20:04:00 -0400

    Moved Test files for Commands package.

--- WARZONE/src/test/java/controller/GamePlayCommandsTest.java
+++ WARZONE/src/test/java/controller/GamePlayCommandsTest.java
@@ -1,8 +1,10 @@
-package controller.middleware.commands;
+package controller;
 
+import controller.middleware.commands.MapEditorCommands;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class GamePlayCommandsTest {
     @Test
