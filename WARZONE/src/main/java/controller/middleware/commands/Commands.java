c508c33ab47ea1ce93e0b8a128b8a8cdcbd5c5cb
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-03-16 14:24:30 -0400

    Update Commands.java

--- WARZONE/src/main/java/controller/middleware/commands/Commands.java
+++ WARZONE/src/main/java/controller/middleware/commands/Commands.java
@@ -3,7 +3,7 @@ package controller.middleware.commands;
 import controller.GameEngine;
 
 public abstract class Commands {
-    private static String[] d_valid_commands;
+    private String[] d_valid_commands;
     final String d_command;
 
     public Commands(String p_command,String[] p_valid_commands)
@@ -15,4 +15,18 @@ public abstract class Commands {
     abstract boolean validateCommand();
 
     abstract void execute(GameEngine ge);
+
+    /**
+     * This validation method only checks if the first token
+     * is a valid command name (doesn't check the rest of the command)
+     * @return true if valid name, false if not
+     */
+    boolean validateCommandName(){
+        for(String l_cmd : d_valid_commands){
+            if(d_command.split(" ")[0].equals(l_cmd)){
+                return true;
+            }
+        }
+        return false;
+    }
 }
