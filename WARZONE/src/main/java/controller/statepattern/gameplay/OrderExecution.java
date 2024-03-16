c6a5f3a5f6fe56a0a9c02aea5faecbefaf5406b1
piyushsatti <piyushsatti@gmail.com>
2024-03-15 22:57:08 -0400

    Created and added IssuerOrder.java and OrderExecution.java into statepattern.

---
+++ WARZONE/src/main/java/controller/statepattern/gameplay/OrderExecution.java
@@ -0,0 +1,30 @@
+package controller.statepattern.gameplay;
+
+import controller.GameEngine;
+import controller.statepattern.Phase;
+
+public class OrderExecution extends Phase {
+    public OrderExecution(GameEngine g) {
+        super(g);
+    }
+
+    @Override
+    public void displayMenu() {
+
+    }
+
+    @Override
+    public void next() {
+
+    }
+
+    @Override
+    public void endGame() {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
