d0d64227e4e0c70913b416151010e129b47b9deb
shamitad <shamita.datta119@gmail.com>
2024-03-17 08:01:40 -0400

    added javadoc for MapEditorCommands

--- WARZONE/src/main/java/controller/middleware/commands/MapEditorCommands.java
+++ WARZONE/src/main/java/controller/middleware/commands/MapEditorCommands.java
@@ -13,7 +13,15 @@ import java.io.IOException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * Represents commands specific to map editing.
+ */
 public class MapEditorCommands extends Commands {
+    /**
+     * Constructor for MapEditorCommands.
+     *
+     * @param p_command The command string.
+     */
     public MapEditorCommands(String p_command) {
         super(p_command, new String[]{
                 "editcontinent",
@@ -27,6 +35,11 @@ public class MapEditorCommands extends Commands {
         });
     }
 
+    /**
+     * Validates the command format.
+     *
+     * @return True if the command is valid, false otherwise.
+     */
     @Override
     public boolean validateCommand() {
         Pattern pattern = Pattern.compile("^editcontinent(?:(?:\\s+-add\\s+\\w+\\s+\\d+)*(?:\\s+-remove\\s+\\w+)*)*(\\s)*$|" +
@@ -41,84 +54,95 @@ public class MapEditorCommands extends Commands {
         return matcher.matches();
     }
 
+
+    /**
+     * Executes the command using the provided GameEngine.
+     *
+     * @param p_worldMap The GameEngine object used to execute the command.
+     */
     @Override
-    public void execute(GameEngine ge) {
+    public void execute(GameEngine p_worldMap) {
         if (!this.validateCommandName()) {
-            ge.d_renderer.renderError("InvalidCommandException : Invalid Command");
+            p_worldMap.d_renderer.renderError("InvalidCommandException : Invalid Command");
             return;
         }
-
 
         String[] l_command = d_command.trim().split("\\s+");
 
         switch (l_command[0]) {
             case "showmap":
-                ge.d_renderer.showMap(false);
+                p_worldMap.d_renderer.showMap(false);
                 break;
             case "validatemap":
-                if (MapInterface.validateMap(ge)) ge.d_renderer.renderMessage("Map is valid");
-                else ge.d_renderer.renderMessage("Map is not valid");
+                if (MapInterface.validateMap(p_worldMap)) p_worldMap.d_renderer.renderMessage("Map is valid");
+                else p_worldMap.d_renderer.renderMessage("Map is not valid");
                 break;
             case "savemap":
                 try {
-                    MapInterface.saveMap(ge, l_command[1]);
+                    MapInterface.saveMap(p_worldMap, l_command[1]);
                 } catch (IOException e) {
-                    ge.d_renderer.renderError("IOException : Encountered File I/O Error");
+                    p_worldMap.d_renderer.renderError("IOException : Encountered File I/O Error");
                 }
                 break;
             case "editmap":
-                editMap(ge);
+                editMap(p_worldMap);
                 break;
             case "editcontinent":
-                if (editContinentValidator(ge.d_worldmap)) {
-                    editContinent(ge.d_worldmap);
+                if (editContinentValidator(p_worldMap.d_worldmap)) {
+                    editContinent(p_worldMap.d_worldmap);
                 }
                 break;
             case "editcountry":
-                if (editCountryValidator(ge.d_worldmap)) {
-                    editCountry(ge.d_worldmap);
+                if (editCountryValidator(p_worldMap.d_worldmap)) {
+                    editCountry(p_worldMap.d_worldmap);
                 }
                 break;
             case "editneighbor":
-                if (editNeighborValidator(ge.d_worldmap)) {
-                    editNeighbor(ge.d_worldmap);
+                if (editNeighborValidator(p_worldMap.d_worldmap)) {
+                    editNeighbor(p_worldMap.d_worldmap);
                 }
                 break;
             case "exit":
-                ge.resetMap();
-                ge.setCurrentState(new Starting(ge));
+                p_worldMap.resetMap();
+                p_worldMap.setCurrentState(new Starting(p_worldMap));
                 break;
         }
     }
 
-    public boolean editContinentValidator(WorldMap wm) {
+    /**
+     * Validates the command format for editing continents.
+     *
+     * @param p_worldMap The WorldMap object representing the game map.
+     * @return True if the command format is valid, false otherwise.
+     */
+    public boolean editContinentValidator(WorldMap p_worldMap) {
         String invalidMessage = "Invalid editcontinent command! Correct format -> editcontinent -add <continentID> <continentvalue> -remove <continentID>";
-        WorldMap copyMap = null;
-        int commandLength = this.splitCommand.length;
+        WorldMap l_copyMap = null;
+        int l_commandLength = this.splitCommand.length;
 
         try {
-            copyMap = new WorldMap(wm);
+            l_copyMap = new WorldMap(p_worldMap);
         } catch (Exception e) {
             System.out.println(e);
             return false;
         }
 
-        if (commandLength < 3) {
+        if (l_commandLength < 3) {
             System.out.println(invalidMessage);
             return false;
         }
 
         int commandIndex = 1;
-        while (commandIndex < commandLength) {
+        while (commandIndex < l_commandLength) {
 
             if (splitCommand[commandIndex].equals("-add")) {
 
-                if (commandIndex + 2 >= commandLength) {
+                if (commandIndex + 2 >= l_commandLength) {
                     System.out.println(invalidMessage);
                     return false;
                 } else {
                     try {
-                        copyMap.addContinent(splitCommand[commandIndex + 1], Integer.parseInt(splitCommand[commandIndex + 2]));
+                        l_copyMap.addContinent(splitCommand[commandIndex + 1], Integer.parseInt(splitCommand[commandIndex + 2]));
                     } catch (Exception e) {
                         System.out.println(e);
                         System.out.println(invalidMessage);
@@ -129,12 +153,12 @@ public class MapEditorCommands extends Commands {
 
             } else if (splitCommand[commandIndex].equals("-remove")) {
 
-                if (commandIndex + 1 >= commandLength) {
+                if (commandIndex + 1 >= l_commandLength) {
                     System.out.println(invalidMessage);
                     return false;
                 } else {
                     try {
-                        copyMap.removeContinent(copyMap.getContinentID(splitCommand[commandIndex + 1]));
+                        l_copyMap.removeContinent(l_copyMap.getContinentID(splitCommand[commandIndex + 1]));
                     } catch (Exception e) {
                         System.out.println(e);
                         System.out.println(invalidMessage);
@@ -153,86 +177,97 @@ public class MapEditorCommands extends Commands {
         return true;
     }
 
-    public void editContinent(WorldMap wm) {
 
-        int commandLength = this.splitCommand.length;
-        int commandIndex = 1;
-        while (commandIndex < commandLength) {
+    /**
+     * Edits the continents of the game map based on the provided command.
+     *
+     * @param p_worldMap The WorldMap object representing the game map.
+     */
+    public void editContinent(WorldMap p_worldMap) {
 
-            if (splitCommand[commandIndex].equals("-add")) {
+        int l_commandLength = this.splitCommand.length;
+        int l_commandIndex = 1;
+        while (l_commandIndex < l_commandLength) {
+
+            if (splitCommand[l_commandIndex].equals("-add")) {
 
                 try {
-                    wm.addContinent(splitCommand[commandIndex + 1], Integer.parseInt(splitCommand[commandIndex + 2]));
+                    p_worldMap.addContinent(splitCommand[l_commandIndex + 1], Integer.parseInt(splitCommand[l_commandIndex + 2]));
                 } catch (Exception e) {
                     System.out.println(e);
                 }
-                commandIndex = commandIndex + 3;
+                l_commandIndex = l_commandIndex + 3;
 
-            } else if (splitCommand[commandIndex].equals("-remove")) {
+            } else if (splitCommand[l_commandIndex].equals("-remove")) {
 
                 try {
-                    wm.removeContinent(wm.getContinentID(splitCommand[commandIndex + 1]));
+                    p_worldMap.removeContinent(p_worldMap.getContinentID(splitCommand[l_commandIndex + 1]));
                 } catch (Exception e) {
                     System.out.println(e);
                 }
-                commandIndex = commandIndex + 2;
+                l_commandIndex = l_commandIndex + 2;
 
             }
         }
 
     }
 
-
-    public boolean editCountryValidator(WorldMap wm) {
+    /**
+     * Validates the command format for editing countries.
+     *
+     * @param p_worldMap The WorldMap object representing the game map.
+     * @return True if the command format is valid, false otherwise.
+     */
+    public boolean editCountryValidator(WorldMap p_worldMap) {
         String invalidMessage = "Invalid editcountry command! Correct format -> editcountry -add <countryID> <continentID> -remove <countryID>";
         WorldMap copyMap = null;
-        int commandLength = this.splitCommand.length;
+        int l_commandLength = this.splitCommand.length;
 
         try {
-            copyMap = new WorldMap(wm);
+            copyMap = new WorldMap(p_worldMap);
         } catch (Exception e) {
             System.out.println(e);
             return false;
         }
 
-        if (commandLength < 3) {
+        if (l_commandLength < 3) {
             System.out.println(invalidMessage);
             return false;
         }
 
-        int commandIndex = 1;
-        while (commandIndex < commandLength) {
+        int l_commandIndex = 1;
+        while (l_commandIndex < l_commandLength) {
 
-            if (splitCommand[commandIndex].equals("-add")) {
+            if (splitCommand[l_commandIndex].equals("-add")) {
 
-                if (commandIndex + 2 >= commandLength) {
+                if (l_commandIndex + 2 >= l_commandLength) {
                     System.out.println(invalidMessage);
                     return false;
                 } else {
                     try {
-                        copyMap.addCountry(splitCommand[commandIndex + 1], wm.getContinentID(splitCommand[commandIndex + 2]), null);
+                        copyMap.addCountry(splitCommand[l_commandIndex + 1], p_worldMap.getContinentID(splitCommand[l_commandIndex + 2]), null);
                     } catch (Exception e) {
                         System.out.println(e);
                         System.out.println(invalidMessage);
                         return false;
                     }
-                    commandIndex = commandIndex + 3;
+                    l_commandIndex = l_commandIndex + 3;
                 }
 
-            } else if (splitCommand[commandIndex].equals("-remove")) {
+            } else if (splitCommand[l_commandIndex].equals("-remove")) {
 
-                if (commandIndex + 1 >= commandLength) {
+                if (l_commandIndex + 1 >= l_commandLength) {
                     System.out.println(invalidMessage);
                     return false;
                 } else {
                     try {
-                        copyMap.removeCountry(wm.getCountryID(splitCommand[commandIndex + 1]));
+                        copyMap.removeCountry(p_worldMap.getCountryID(splitCommand[l_commandIndex + 1]));
                     } catch (Exception e) {
                         System.out.println(e);
                         System.out.println(invalidMessage);
                         return false;
                     }
-                    commandIndex = commandIndex + 2;
+                    l_commandIndex = l_commandIndex + 2;
                 }
 
             } else {
@@ -245,62 +280,72 @@ public class MapEditorCommands extends Commands {
         return true;
     }
 
+    /**
+     * Edits the countries of the game map based on the provided command.
+     *
+     * @param p_worldMap The WorldMap object representing the game map.
+     */
+    public void editCountry(WorldMap p_worldMap) {
+        int l_commandLength = this.splitCommand.length;
 
-    public void editCountry(WorldMap wm) {
-        int commandLength = this.splitCommand.length;
 
+        int l_commandIndex = 1;
+        while (l_commandIndex < l_commandLength) {
 
-        int commandIndex = 1;
-        while (commandIndex < commandLength) {
-
-            if (splitCommand[commandIndex].equals("-add")) {
+            if (splitCommand[l_commandIndex].equals("-add")) {
 
                 try {
-                    wm.addCountry(splitCommand[commandIndex + 1], wm.getContinentID(splitCommand[commandIndex + 2]), null);
+                    p_worldMap.addCountry(splitCommand[l_commandIndex + 1], p_worldMap.getContinentID(splitCommand[l_commandIndex + 2]), null);
                 } catch (Exception e) {
                     System.out.println(e);
                     return;
                 }
-                commandIndex = commandIndex + 3;
+                l_commandIndex = l_commandIndex + 3;
 
 
-            } else if (splitCommand[commandIndex].equals("-remove")) {
+            } else if (splitCommand[l_commandIndex].equals("-remove")) {
 
                 try {
-                    wm.removeCountry(wm.getCountryID(splitCommand[commandIndex + 1]));
+                    p_worldMap.removeCountry(p_worldMap.getCountryID(splitCommand[l_commandIndex + 1]));
                 } catch (Exception e) {
                     System.out.println(e);
                     return;
                 }
-                commandIndex = commandIndex + 2;
+                l_commandIndex = l_commandIndex + 2;
             }
         }
     }
 
-    public boolean editNeighborValidator(WorldMap wm) {
+    /**
+     * Validates the command format for editing country neighbors.
+     *
+     * @param p_worldMap The WorldMap object representing the game map.
+     * @return True if the command format is valid, false otherwise.
+     */
+    public boolean editNeighborValidator(WorldMap p_worldMap) {
         String invalidMessage = "Invalid editneighbor command! Correct format -> editneighbor -add <countryID> <neighborcountryID> " +
                 "-remove <countryID> <neighborcountryID>";
         WorldMap copyMap = null;
-        int commandLength = this.splitCommand.length;
+        int l_commandLength = this.splitCommand.length;
 
         try {
-            copyMap = new WorldMap(wm);
+            copyMap = new WorldMap(p_worldMap);
         } catch (Exception e) {
             System.out.println(e);
             return false;
         }
 
-        if (commandLength < 3) {
+        if (l_commandLength < 3) {
             System.out.println(invalidMessage);
             return false;
         }
 
         int commandIndex = 1;
-        while (commandIndex < commandLength) {
+        while (commandIndex < l_commandLength) {
 
             if (splitCommand[commandIndex].equals("-add")) {
 
-                if (commandIndex + 2 >= commandLength) {
+                if (commandIndex + 2 >= l_commandLength) {
                     System.out.println(invalidMessage);
                     return false;
                 } else {
@@ -316,7 +361,7 @@ public class MapEditorCommands extends Commands {
 
             } else if (splitCommand[commandIndex].equals("-remove")) {
 
-                if (commandIndex + 2 >= commandLength) {
+                if (commandIndex + 2 >= l_commandLength) {
                     System.out.println(invalidMessage);
                     return false;
                 } else {
@@ -341,17 +386,20 @@ public class MapEditorCommands extends Commands {
     }
 
 
-    public void editNeighbor(WorldMap wm) {
+    /**
+     * Edits the neighbors of countries on the game map based on the provided command.
+     *
+     * @param p_worldMap The WorldMap object representing the game map.
+     */
+    public void editNeighbor(WorldMap p_worldMap) {
         int commandLength = this.splitCommand.length;
-
-
         int commandIndex = 1;
         while (commandIndex < commandLength) {
 
             if (splitCommand[commandIndex].equals("-add")) {
 
                 try {
-                    wm.addBorder(wm.getCountryID(splitCommand[commandIndex+1]),wm.getCountryID(splitCommand[commandIndex+2]));
+                    p_worldMap.addBorder(p_worldMap.getCountryID(splitCommand[commandIndex+1]),p_worldMap.getCountryID(splitCommand[commandIndex+2]));
                 } catch (Exception e) {
                     System.out.println(e);
                     return;
@@ -362,7 +410,7 @@ public class MapEditorCommands extends Commands {
             } else if (splitCommand[commandIndex].equals("-remove")) {
 
                 try {
-                    wm.removeBorder(wm.getCountryID(splitCommand[commandIndex+1]),wm.getCountryID(splitCommand[commandIndex+2]));
+                    p_worldMap.removeBorder(p_worldMap.getCountryID(splitCommand[commandIndex+1]),p_worldMap.getCountryID(splitCommand[commandIndex+2]));
                 } catch (Exception e) {
                     System.out.println(e);
                     return;
@@ -372,31 +420,41 @@ public class MapEditorCommands extends Commands {
         }
     }
 
-    public void editMap(GameEngine ge) {
+
+    /**
+     * Edits the game map based on the provided command to load, save, or modify the map.
+     *
+     * @param p_gameEngine The GameEngine object managing the game.
+     */
+    public void editMap(GameEngine p_gameEngine) {
         String mapName = "";
         if (this.splitCommand.length < 2) {
-            ge.d_renderer.renderError("Invalid command format! Correct format -> editmap <mapname>");
+            p_gameEngine.d_renderer.renderError("Invalid command format! Correct format -> editmap <mapname>");
             return;
         }
 
         mapName = splitCommand[1];
 
         try {
-            MapInterface.loadMap2(ge, mapName);
-        } catch (FileNotFoundException e) {
-            ge.d_renderer.renderError("FileNotFoundException : File does not exist.");
-            ge.d_renderer.renderMessage("Creating file by the name : " + mapName);
+            MapInterface.loadMap2(p_gameEngine, mapName);
+        }
+        catch (FileNotFoundException e) {
+            p_gameEngine.d_renderer.renderError("FileNotFoundException : File does not exist.");
+            p_gameEngine.d_renderer.renderMessage("Creating file by the name : " + mapName);
             try {
-                MapInterface.saveMap(ge, mapName);
-            } catch (IOException ex) {
+                MapInterface.saveMap(p_gameEngine, mapName);
+            }
+            catch (IOException ex) {
                 throw new RuntimeException(ex);
             }
-            editMap(ge);
-        } catch (NumberFormatException e) {
-            ge.d_renderer.renderError("NumberFormatException : File has incorrect formatting.");
-        } catch (ContinentAlreadyExistsException | ContinentDoesNotExistException |
+            editMap(p_gameEngine);
+        }
+        catch (NumberFormatException e) {
+            p_gameEngine.d_renderer.renderError("NumberFormatException : File has incorrect formatting.");
+        }
+        catch (ContinentAlreadyExistsException | ContinentDoesNotExistException |
                  DuplicateCountryException | CountryDoesNotExistException e) {
-            ge.d_renderer.renderError("InvalidMapException : Map is disjoint or incorrect.");
+            p_gameEngine.d_renderer.renderError("InvalidMapException : Map is disjoint or incorrect.");
         }
     }
 
