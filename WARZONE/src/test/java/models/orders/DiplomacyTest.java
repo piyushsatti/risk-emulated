71b7670ee8ceceb132e09fd87dc77e14998874c4
priyanshu7175 <priyanshuadhikari7@gmail.com>
2024-03-17 19:51:01 -0400

    Junit for DiplomacyTest

--- WARZONE/src/test/java/models/orders/DiplomacyTest.java
+++ WARZONE/src/test/java/models/orders/DiplomacyTest.java
@@ -51,7 +51,7 @@ public class DiplomacyTest {
     }
 
     @Test
-    public void orderValidationTest3() throws CountryDoesNotExistException, ContinentAlreadyExistsException, ContinentDoesNotExistException, DuplicateCountryException, FileNotFoundException, InvalidCommandException {
+    public void orderValidationTest1() throws CountryDoesNotExistException, ContinentAlreadyExistsException, ContinentDoesNotExistException, DuplicateCountryException, FileNotFoundException, InvalidCommandException {
         GameEngine ge = new GameEngine();
         MapInterface.loadMap2(ge,"usa9.map");
         ge.d_players.add(new Player("Priyanshu",ge));
@@ -73,6 +73,31 @@ public class DiplomacyTest {
         b.execute();
 
         assertEquals(4,c.getReinforcements());
+    }
+
+    @Test
+    public void orderValidationTest2() throws CountryDoesNotExistException, ContinentAlreadyExistsException, ContinentDoesNotExistException, DuplicateCountryException, FileNotFoundException, InvalidCommandException {
+        GameEngine ge = new GameEngine();
+        MapInterface.loadMap2(ge,"usa9.map");
+        ge.d_players.add(new Player("Priyanshu",ge));
+        ge.d_players.add(new Player("Abc",ge));
+
+        StartupCommands cmd = new StartupCommands("assigncountries");
+        cmd.execute(ge);
+        Reinforcement rf = new Reinforcement(ge);
+        rf.run();
+
+        Deploy deploy = new Deploy(ge.d_players.get(0),ge.d_players.get(0).getName(),ge.d_players.get(0).getPlayerId(),ge.d_players.get(0).getAssignedCountries().get(0),10,ge);
+        deploy.execute();
+
+        Diplomacy dip = new Diplomacy(ge.d_players.get(0),ge.d_players.get(1),ge.d_players.get(0).getPlayerId(),ge.d_players.get(0).getName());
+        dip.execute();
+
+        Country c =ge.d_worldmap.getCountry(ge.d_players.get(0).getAssignedCountries().get(0));
+        Bomb b = new Bomb(ge.d_players.get(1),ge.d_players.get(0),ge.d_players.get(1).getPlayerId(),ge.d_players.get(1).getName(), ge.d_players.get(0).getAssignedCountries().get(0),ge);
+        b.execute();
+
+        assertNotEquals(5,c.getReinforcements());
     }
 
 }
