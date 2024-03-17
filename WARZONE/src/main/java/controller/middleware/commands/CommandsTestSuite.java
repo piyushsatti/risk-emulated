78910aa7ef6b6f7e97568916b278c46f28109ef6
shamitad <shamita.datta119@gmail.com>
2024-03-17 08:22:13 -0400

    added javadoc for IssueOrderCommandsCommands

--- WARZONE/src/main/java/controller/middleware/commands/CommandsTestSuite.java
+++ WARZONE/src/main/java/controller/middleware/commands/CommandsTestSuite.java
@@ -4,6 +4,10 @@ import controller.MapInterface;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * Test suite for testing  MapEditorCommands, MapInterface, IssueOrderCommands, and StartupCommands.
+ *
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({MapEditorCommands.class, MapInterface.class, IssueOrderCommands.class, StartupCommands.class})
 public class CommandsTestSuite {
