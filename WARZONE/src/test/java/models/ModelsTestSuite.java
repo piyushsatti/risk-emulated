86f1f0e7856af220f39c08eb14df1e686ece430a
priyanshu7175 <priyanshuadhikari7@gmail.com>
2024-03-17 20:37:37 -0400

    JSuite WorldMapTest file deleted

--- WARZONE/src/test/java/models/ModelsTestSuite.java
+++ WARZONE/src/test/java/models/ModelsTestSuite.java
@@ -1,11 +1,10 @@
 package models;
 
 import models.orders.OrdersTestSuite;
-import models.worldmap.WorldMapTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({OrdersTestSuite.class, WorldMapTestSuite.class})
+@Suite.SuiteClasses({OrdersTestSuite.class})
 public class ModelsTestSuite {
 }
--- WARZONE/src/test/java/models/worldmap/WorldMapTestSuite.java
+++
@@ -1,9 +0,0 @@
-package models.worldmap;
-
-import org.junit.runner.RunWith;
-import org.junit.runners.Suite;
-
-@RunWith(Suite.class)
-@Suite.SuiteClasses({WorldMapTest.class})
-public class WorldMapTestSuite {
-}
