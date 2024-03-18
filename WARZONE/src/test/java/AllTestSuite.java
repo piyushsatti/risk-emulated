9b4ce52ece5025e6cf6e95bd8084e395425bbf71
priyanshu7175 <priyanshuadhikari7@gmail.com>
2024-03-17 20:57:58 -0400

    Adding Javadoc for all

--- WARZONE/src/test/java/AllTestSuite.java
+++ WARZONE/src/test/java/AllTestSuite.java
@@ -3,6 +3,9 @@ import models.ModelsTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * Javadoc for the AllTestSuite class.
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ControllerTestSuite.class, ModelsTestSuite.class})
 public class AllTestSuite {
--- WARZONE/src/test/java/controller/ControllerTestSuite.java
+++ WARZONE/src/test/java/controller/ControllerTestSuite.java
@@ -5,7 +5,11 @@ import controller.statepattern.gameplay.GamePlayTestSuite;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
+/**
+ * Javadoc for the ControllerTestSuite class.
+ */
 @RunWith(Suite.class)
 @Suite.SuiteClasses({MapInterfaceTest.class, CommandsTestSuite.class, GamePlayTestSuite.class})
 public class ControllerTestSuite {
 }
+
