2eff5e9b442436433924c16cb56543f71655cd16
piyushsatti <piyushsatti@gmail.com>
2024-03-17 20:37:55 -0400

    Observer.java and Order.java interface integration.

--- WARZONE/src/main/java/models/orders/Order.java
+++ WARZONE/src/main/java/models/orders/Order.java
@@ -6,72 +6,19 @@ import helpers.exceptions.InvalidCommandException;
  * The Order class represents a player's order in the game.
  * It specifies the movement of reinforcements from one country to another.
  */
-//public abstract class Order {
-//
-//    int d_playerOrderID;
-//
-//    String d_playerOrderName;
-//
-//    private final int d_fromCountryID;
-//
-//    private final int d_toCountryID;
-//
-//    private final int d_reinforcementsDeployed;
-//
-//    /**
-//     * Constructs an Order object with specified attributes.
-//     *
-//     * @param p_playerOrderName The name of the player issuing the order.
-//     * @param p_playerOrderID The ID of the player issuing the order.
-//     * @param p_fromCountryID The ID of the country from which reinforcements are deployed.
-//     * @param p_reinforcementsDeployed The number of reinforcements deployed.
-//     */
-//    public Order(String p_playerOrderName, int p_playerOrderID,int p_fromCountryID,int p_reinforcementsDeployed){
-//
-//        this.d_playerOrderName = p_playerOrderName;
-//
-//        this.d_playerOrderID = p_playerOrderID;
-//
-//        this.d_fromCountryID = p_fromCountryID;
-//
-//        this.d_toCountryID = -1;
-//
-//        this.d_reinforcementsDeployed = p_reinforcementsDeployed;
-//
-//    }
-//
-//    /**
-//     * Executes the order by deploying reinforcements to the specified country.
-//     */
-//  public  abstract void execute();//{
-////
-////        int l_currentReinforcements = GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).getReinforcements();
-////
-////        GameEngine.CURRENT_MAP
-////                .getCountry(this.d_fromCountryID)
-////                .setReinforcements(this.d_reinforcementsDeployed + l_currentReinforcements);
-////
-////        TerminalRenderer.renderMessage(
-////                "Order Executed: " +
-////                        this.d_reinforcementsDeployed +
-////                        " troops deployed on " +
-////                        GameEngine.CURRENT_MAP.getCountry(this.d_fromCountryID).getCountryName() +
-////                        " by " +
-////                        this.d_playerOrderName
-////        );
-////
-////    }
-//
-//}
 
 public interface Order {
 
-
+    /**
+     * Validates the command for the order.
+     *
+     * @return True if the command is valid, false otherwise
+     * @throws InvalidCommandException if the command is invalid
+     */
     public boolean validateCommand() throws InvalidCommandException;
 
     /**
      * used to execute the order. Is implemented by the Concrete Command classes.
-     * @return true if execution is successful, false if it fails
      */
     public void execute();
 
