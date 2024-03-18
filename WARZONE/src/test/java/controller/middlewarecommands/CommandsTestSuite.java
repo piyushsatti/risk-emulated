e34ef20900241db85b8ab9bb2117222da97c35db
piyushsatti <piyushsatti@gmail.com>
2024-03-17 20:25:58 -0400

    CommandsTestSuite.java merged.

---
+++ WARZONE/src/test/java/controller/middlewarecommands/CommandsTestSuite.java
@@ -0,0 +1,9 @@
+package controller.middlewarecommands;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({MapEditorCommandsTest.class, StartupCommandsTest.class, IssueOrderCommandsTest.class})
+public class CommandsTestSuite {
+}
