7f001a51715623082eae4b6b3d4ea1464656623d
Eden Almakias <eden.almakias@gmail.com>
2024-04-04 14:26:26 -0400

    Implemented ConquestMapInterface adaptee in the MapAdapter

--- WARZONE/src/main/java/controller/MapFileManagement/MapAdapter.java
+++ WARZONE/src/main/java/controller/MapFileManagement/MapAdapter.java
@@ -1,4 +1,25 @@
 package controller.MapFileManagement;
 
+import controller.GameEngine;
+import models.worldmap.WorldMap;
+
 public class MapAdapter extends MapInterface{
+
+    ConquestMapInterface d_adaptee;
+
+    public MapAdapter(ConquestMapInterface p_cmi){
+        d_adaptee = p_cmi;
+    }
+
+
+    @Override
+    public void saveMap(GameEngine p_gameEngine, String p_FileName){
+        d_adaptee.saveConquestMap(p_gameEngine, p_FileName);
+    }
+
+    @Override
+    public WorldMap loadMap(GameEngine p_gameEngine, MapFileLoader p_mfl){
+        return d_adaptee.loadConquestMap(p_gameEngine, p_mfl);
+    }
+
 }
