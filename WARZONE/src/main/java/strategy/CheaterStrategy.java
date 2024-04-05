b9c03fb9e468beefa6d127fe4395e2d2d6ad3b3a
priyanshu7175 <priyanshuadhikari7@gmail.com>
2024-04-05 15:42:22 -0400

    Cheater Strategy

--- WARZONE/src/main/java/strategy/CheaterStrategy.java
+++ WARZONE/src/main/java/strategy/CheaterStrategy.java
@@ -1,15 +1,72 @@
 package strategy;
 
+import controller.GameEngine;
+import models.Player;
+import models.orders.Deploy;
+import models.orders.Order;
 import models.worldmap.Country;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class CheaterStrategy implements Strategy{
-    @Override
+
+    private Player d_player;
+
+    private GameEngine d_gameEngine;
+    public CheaterStrategy(Player p_player, GameEngine p_gameEngine) {
+        this.d_player = p_player;
+        this.d_gameEngine = p_gameEngine;
+    }
     public int getSourceCountry() {
-        return 0;
+        return this.d_player.getAssignedCountries().get(0);
     }
 
     @Override
     public int getTargetCountry(int p_sourceCountryId) {
         return 0;
+    }
+
+    public ArrayList<Integer> getNeighbouringCountry(int p_sourceCountry){
+
+        ArrayList<Integer> l_listOfAllBorderCountriesIDs = new ArrayList<>();
+        for(Integer id : d_gameEngine.d_worldmap.getCountry(p_sourceCountry).getAllBorderCountriesIDs()){
+            if(!this.d_player.getAssignedCountries().contains(id)){
+            l_listOfAllBorderCountriesIDs.add(id);}
+        }
+        return l_listOfAllBorderCountriesIDs;
+    }
+
+
+    public Order createOrder(){
+
+        int l_sourceCountry = getSourceCountry();
+        ArrayList<Integer> l_neighbourCountries=getNeighbouringCountry(l_sourceCountry);
+
+        for(int l_countryId:l_neighbourCountries){
+            boolean l_isNeutral = true;
+
+            for(Player l_player:this.d_gameEngine.d_players){
+
+                if(l_player.getAssignedCountries().contains(l_countryId)){
+                    l_isNeutral = false;
+                 l_player.removeAssignedCountries(l_countryId);
+                 this.d_player.setAssignedCountries(l_countryId);
+                 int l_armies=this.d_gameEngine.d_worldmap.getCountry(l_countryId).getReinforcements();
+                 this.d_gameEngine.d_worldmap.getCountry(l_countryId).setReinforcements(l_armies*2);
+                 break;
+                }
+
+            }
+
+            if(l_isNeutral){
+                this.d_player.setAssignedCountries(l_countryId);
+                int l_armies=this.d_gameEngine.d_worldmap.getCountry(l_countryId).getReinforcements();
+                this.d_gameEngine.d_worldmap.getCountry(l_countryId).setReinforcements(l_armies*2);
+            }
+        }
+
+
+        return null;
     }
 }
