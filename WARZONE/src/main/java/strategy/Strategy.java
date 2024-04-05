b88de39e378b798b64628a50b46bc123a1d72e26
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-05 18:56:57 -0400

    Update Strategy.java

--- WARZONE/src/main/java/strategy/Strategy.java
+++ WARZONE/src/main/java/strategy/Strategy.java
@@ -1,6 +1,9 @@
 package strategy;
 
+import models.orders.Order;
+
 public interface Strategy {
      int getSourceCountry();
      int getTargetCountry(int p_sourceCountryId);
+     Order createOrder();
 }
