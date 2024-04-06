a3d52cb4227cfe1c56222e0f41f4c287e84cf9e2
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-06 17:30:35 -0400

    Create CommandsTestSuite.java

---
+++ WARZONE/src/test/java/controller/middleware/commands/CommandsTestSuite.java
@@ -0,0 +1,12 @@
+package controller.middleware.commands;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Javadoc for the CommandsTestSuite class.
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({TournamentTest.class,MapEditorCommandsTest.class, StartupCommandsTest.class, IssueOrderCommandsTest.class})
+public class CommandsTestSuite {
+}
