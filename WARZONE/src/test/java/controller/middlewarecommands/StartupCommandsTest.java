1d99cb83c39274c86280354fc576f24897aec8c6
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-06 16:41:32 -0400

    Update StartupCommandsTest.java
    adding test cases to test along with javadoc comments

--- WARZONE/src/test/java/controller/middlewarecommands/StartupCommandsTest.java
+++ WARZONE/src/test/java/controller/middlewarecommands/StartupCommandsTest.java
@@ -221,4 +221,145 @@ public class StartupCommandsTest {
         StartupCommands obj = new StartupCommands(cmd);
         assertTrue(obj.validateCommand(d_gameEngine));
     }
+
+    /**
+     * Test cases for validating the tournament command with incorrect command
+     */
+    @Test
+    public void testTournament1()
+    {
+        String cmd = "tournament -M devdutt usa1.map usa2.map pikachu3.map pikachu4.map -P cheater human -G 1 -D 10";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertFalse(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament2()
+    {
+        String cmd = "tournament -M devdutt.map usa1.map usa2.map pikachu3.map pikachu4.map -P cheater human -G 1 -D 10";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament3()
+    {
+        String cmd = "tournament -M devdutt.map usa1.map usa2.map pikachu3.map -P cheater human -G 1 -D 10";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament4()
+    {
+        String cmd = "tournament -M devdutt.map usa1.map usa2.map  -P cheater human -G 1 -D 10";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament6()
+    {
+        String cmd = "tournament -M devdutt.map   -P cheater human -G 1 -D 10";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament7()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map   -P cheater human -G 1 -D 10";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with incorrect command
+     */
+    @Test
+    public void testTournament8()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu shashi.map   -P cheater human -G 1 -D 10";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertFalse(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with incorrect command
+     */
+    @Test
+    public void testTournament9()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map shashi.map   -P cheater human -G 1 -D 100";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertFalse(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with incorrect command
+     */
+    @Test
+    public void testTournament10()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map shashi.map   -P  human -G 1 -D 12";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertFalse(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament11()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map shashi.map   -P  human cheater random aggressive -G 1 -D 12";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with incorrect command
+     */
+    @Test
+    public void testTournament12()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map shashi.map   -P  human cheater random aggressive benevolent -G 1 -D 12";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertFalse(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament13()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map shashi.map   -P  human cheater random aggressive -G 5 -D 12";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with incorrect command
+     */
+    @Test
+    public void testTournament14()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map shashi.map   -P  human cheater random aggressive -G 6 -D 12";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertFalse(obj.validateCommand(d_gameEngine));
+    }
+    /**
+     * Test cases for validating the tournament command with correct command
+     */
+    @Test
+    public void testTournament15()
+    {
+        String cmd = "tournament -M devdutt.map priyanshu.map shashi.map   -P  human cheater random aggressive -G  4 -D 12";
+        StartupCommands obj = new StartupCommands(cmd);
+        assertTrue(obj.validateCommand(d_gameEngine));
+    }
 }
