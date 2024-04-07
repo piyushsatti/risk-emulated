c6f0d488aa7476b102e9be4b9fd6f7126c7dbde8
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-06 20:18:30 -0400

    Update Tournament.java

--- WARZONE/src/main/java/controller/statepattern/Tournament.java
+++ WARZONE/src/main/java/controller/statepattern/Tournament.java
@@ -11,7 +11,9 @@ public class Tournament extends Phase{
      * @param gameEngine The GameEngine object managing the game.
      */
     public Tournament(GameEngine gameEngine) {
+
         super(gameEngine);
+        d_phaseName = "Tournament Phase";
     }
 
     @Override
