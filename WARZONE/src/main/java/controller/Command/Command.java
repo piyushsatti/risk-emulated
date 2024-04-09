dd2e611b633bfd90633b99c17268b8971aaac44e
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-09 01:34:06 -0400

    Update Command.java

--- WARZONE/src/main/java/controller/Command/Command.java
+++ WARZONE/src/main/java/controller/Command/Command.java
@@ -42,10 +42,11 @@ public abstract class Command {
 
     /**
      * Constructs a Command object with specified input and game engine.
+     *
      * @param p_input The input string for the command.
-     * @param p_ge The game engine to operate on.
+     * @param p_ge    The game engine to operate on.
      */
-    protected Command(String p_input, GameEngine p_ge){
+    protected Command(String p_input, GameEngine p_ge) {
         d_input = p_input;
         d_ge = p_ge;
         d_splitCommand = d_input.split("\\s+");
@@ -53,15 +54,17 @@ public abstract class Command {
 
     /**
      * Validates the command by checking its phase, format, and logic.
+     *
      * @return true if the command is valid, false otherwise.
      */
-    public boolean validate(){
-        if(validatePhase() && validateFormat()){
+    public boolean validate() {
+        if (validatePhase() && validateFormat()) {
             return validateLogic();
-        }else{
+        } else {
             return false;
         }
     }
+
     /**
      * Executes the command.
      */
@@ -69,30 +72,34 @@ public abstract class Command {
 
     /**
      * Validates the format of the command based on its pattern.
+     *
      * @return true if the format is valid, false otherwise.
      */
-    public  boolean validateFormat(){
+    public boolean validateFormat() {
         Matcher l_matcher = d_commandPattern.matcher(d_input);
-        if(l_matcher.matches()){
+        if (l_matcher.matches()) {
             return true;
-        }else{
+        } else {
             invalidFormatMessage();
             return false;
         }
     }
+
     /**
      * Validates the logic of the command.
+     *
      * @return true if the logic is valid, false otherwise.
      */
     public abstract boolean validateLogic();
 
     /**
      * Validates the phase of the command based on the current game state.
+     *
      * @return true if the command is valid for the current phase, false otherwise.
      */
-    public boolean validatePhase(){
-        for(Class l_c : d_validPhases){
-            if(l_c == d_ge.getCurrentState().getClass()) return true;
+    public boolean validatePhase() {
+        for (Class l_c : d_validPhases) {
+            if (l_c == d_ge.getCurrentState().getClass()) return true;
         }
         this.d_ge.d_renderer.renderError("Invalid command in this phase!");
         return false;
@@ -101,7 +108,7 @@ public abstract class Command {
     /**
      * Displays an error message for invalid command format.
      */
-    public void invalidFormatMessage(){
+    public void invalidFormatMessage() {
         this.d_ge.d_renderer.renderError("Invalid command! Correct format -> " + d_validCommandFormat);
     }
 
