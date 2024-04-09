55f5c69982a116bf49862bf45f7d39445a87d14b
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-09 12:57:06 -0400

    Update MapInterface.java

--- WARZONE/src/main/java/controller/MapFileManagement/MapInterface.java
+++ WARZONE/src/main/java/controller/MapFileManagement/MapInterface.java
@@ -20,6 +20,16 @@ import java.util.Scanner;
 public class MapInterface {
 
     /**
+     * Represents a buffer for storing log entries.
+     */
+    LogEntryBuffer d_logEntryBuffer = new LogEntryBuffer();
+
+    /**
+     * Represents a logger associated with a log entry buffer.
+     */
+    Logger d_lw = new Logger(d_logEntryBuffer);
+
+    /**
      * Creates a File object from the specified file name within the game engine's maps folder.
      *
      * @param p_gameEngine The game engine instance.
@@ -35,6 +45,7 @@ public class MapInterface {
             return l_map_file_obj;
         } else {
             p_gameEngine.d_renderer.renderMessage("File does not exist");
+            d_logEntryBuffer.setString("File Not Found. it does not exist");
             throw new FileNotFoundException("File does not exist.");
         }
     }
@@ -42,23 +53,25 @@ public class MapInterface {
     /**
      * Saves the provided map to a file with the given file name.
      *
-     * @param p_gameEnginee the game engine associated with the map.
-     * @param p_FileName    The name of the file to save the map to.
+     * @param p_gameEngine the game engine associated with the map.
+     * @param p_FileName   The name of the file to save the map to.
      * @throws IOException If an I/O error occurs while writing to the file.
      */
-    public void saveMap(GameEngine p_gameEnginee, String p_FileName){
-        WorldMap p_map = p_gameEnginee.d_worldmap;
+    public void saveMap(GameEngine p_gameEngine, String p_FileName) {
+        WorldMap p_map = p_gameEngine.d_worldmap;
         p_map.normalizeContinents();
-        File outputFile = new File(p_gameEnginee.d_maps_folder + p_FileName);
-
+        File l_outputFile = new File(p_gameEngine.d_maps_folder + p_FileName);
+        //creating a new output file
         try {
-            p_gameEnginee.d_renderer.renderMessage("Was file created? " + outputFile.createNewFile());
-        }catch (Exception e){
-            p_gameEnginee.d_renderer.renderError("Save map failed!");
+            p_gameEngine.d_renderer.renderMessage("Was file created? " + l_outputFile.createNewFile());
+        } catch (Exception e) {
+            p_gameEngine.d_renderer.renderError("Save map failed!");
+            p_gameEngine.d_renderer.renderError("IOException : Encountered File I/O Error");
+            d_logEntryBuffer.setString("\nPhase: MapEditor \n  Command: savemap Not Executed due to File I/O Error");
             return;
         }
-
-        String file_signature = """
+        //the file signature for the output file
+        String l_file_signature = """
                 ; map: estonia.map
                 ; map made with the map maker
                 ; yura.net Risk 1.0.9.3
@@ -68,81 +81,79 @@ public class MapInterface {
                 map estonia_map.gif
                 crd estonia.cards
                 """;
-        BufferedWriter writer = null;
+        BufferedWriter l_writer = null;
 
         try {
-            writer = new BufferedWriter(new FileWriter(outputFile));
-        }
-        catch(Exception e){
-            p_gameEnginee.d_renderer.renderError("Save map failed!");
+            l_writer = new BufferedWriter(new FileWriter(l_outputFile));
+        } catch (Exception e) {
+            p_gameEngine.d_renderer.renderError("Save map failed!");
+            d_logEntryBuffer.setString("\nSaveMap failed!");
             return;
         }
-
-        StringBuilder added_line = new StringBuilder();
-        added_line.append(file_signature);
-        added_line.append("\n[continents]\n");
-
-        for (Continent continent_obj : p_map.getContinents().values()) {
-            added_line.append(continent_obj.d_continentName)
-                    .append(" ").append(continent_obj.getBonus())
+        //from here, we are adding the map contents to the output file.
+        StringBuilder l_added_line = new StringBuilder();
+        l_added_line.append(l_file_signature);
+        l_added_line.append("\n[continents]\n");
+        //adding continents
+        for (Continent l_continent_obj : p_map.getContinents().values()) {
+            l_added_line.append(l_continent_obj.d_continentName)
+                    .append(" ").append(l_continent_obj.getBonus())
                     .append(" ").append("xxx").append("\n");
         }
 
-        added_line.append("\n[countries]\n");
-
-        for (Country country_obj : p_map.getCountries().values()) {
-            added_line.append(country_obj.getCountryID())
-                    .append(" ").append(country_obj.getCountryName())
-                    .append(" ").append(country_obj.getContinent().getContinentID())
+        l_added_line.append("\n[countries]\n");
+        //adding countries
+        for (Country l_country_obj : p_map.getCountries().values()) {
+            l_added_line.append(l_country_obj.getCountryID())
+                    .append(" ").append(l_country_obj.getCountryName())
+                    .append(" ").append(l_country_obj.getContinent().getContinentID())
                     .append(" 0 0").append("\n");
         }
-        added_line.append("\n[borders]");
+        l_added_line.append("\n[borders]");
 
-        for (Integer country_id : p_map.getCountries().keySet()) {
-            HashMap<Integer, Country> border_countries = p_map.getCountry(country_id).getBorderCountries();
-            added_line.append("\n");
-            added_line.append(country_id);
+        for (Integer l_country_id : p_map.getCountries().keySet()) {
+            HashMap<Integer, Country> l_border_countries = p_map.getCountry(l_country_id).getBorderCountries();
+            l_added_line.append("\n");
+            l_added_line.append(l_country_id);
 
-            for (Integer border_country_id : border_countries.keySet()) {
-                added_line.append(" ").append(border_country_id);
+            for (Integer l_border_country_id : l_border_countries.keySet()) {
+                l_added_line.append(" ").append(l_border_country_id);
             }
         }
-
         try {
-            writer.write(added_line.toString());
-            writer.close();
-        }catch (Exception e){
-            p_gameEnginee.d_renderer.renderError("Save map failed!");
+            l_writer.write(l_added_line.toString());
+            l_writer.close();
+        } catch (Exception e) {
+            p_gameEngine.d_renderer.renderError("Save map failed!");
+            d_logEntryBuffer.setString("\nSaveMap failed!");
             return;
         }
-
-        LogEntryBuffer logEntryBuffer = new LogEntryBuffer();
-        Logger lw = new Logger(logEntryBuffer);
-        logEntryBuffer.setString("saved map :" + p_FileName);
+        d_logEntryBuffer.setString(" \nsaved map :" + p_FileName);
     }
 
     /**
      * Loads a map into the game engine.
      *
      * @param p_gameEngine The GameEngine object.
-     * @param mapName      The name of the map to load.
+     * @param p_mapName    The name of the map to load.
      * @throws FileNotFoundException           If the specified file is not found.
      * @throws ContinentAlreadyExistsException If a continent already exists while loading.
      * @throws ContinentDoesNotExistException  If a continent does not exist while loading.
      * @throws DuplicateCountryException       If a country already exists while loading.
      * @throws CountryDoesNotExistException    If a country does not exist while loading.
      */
-    public WorldMap loadMap(GameEngine p_gameEngine, String mapName) throws FileNotFoundException, ContinentAlreadyExistsException, ContinentDoesNotExistException, DuplicateCountryException, CountryDoesNotExistException {
+    public WorldMap loadMap(GameEngine p_gameEngine, String p_mapName) throws FileNotFoundException, ContinentAlreadyExistsException, ContinentDoesNotExistException, DuplicateCountryException, CountryDoesNotExistException {
         WorldMap l_worldMap = new WorldMap();
         File l_map_file_obj;
         Scanner l_file_reader;
-        l_map_file_obj = createFileObjectFromFileName(p_gameEngine, mapName);
+        //creating a map file object
+        l_map_file_obj = createFileObjectFromFileName(p_gameEngine, p_mapName);
         l_file_reader = new Scanner(l_map_file_obj);
         String[] l_split_data;
         while (l_file_reader.hasNextLine()) {
 
             l_split_data = l_file_reader.nextLine().split(" ");
-
+            //loading the continents,countries and borders
             switch (l_split_data[0]) {
                 case "[continents]" -> loadContinents(l_file_reader, l_worldMap);
                 case "[countries]" -> loadCountries(l_file_reader, l_worldMap);
@@ -155,27 +166,27 @@ public class MapInterface {
 
     /**
      * Loads a map into the game engine from a file using the provided map file loader.
+     *
      * @param p_gameEngine The game engine to load the map into.
-     * @param mfl The map file loader containing the map file.
+     * @param p_mfl        The map file loader containing the map file.
      * @return The loaded world map.
      */
-    public WorldMap loadMap(GameEngine p_gameEngine, MapFileLoader mfl) {
+    public WorldMap loadMap(GameEngine p_gameEngine, MapFileLoader p_mfl) {
 
         WorldMap l_worldMap = new WorldMap();
         Scanner l_file_reader = null;
-
         try {
-            l_file_reader = new Scanner(mfl.d_mapFile);
+            l_file_reader = new Scanner(p_mfl.d_mapFile);
         } catch (Exception e) {
             p_gameEngine.d_renderer.renderError("File does not exist!");
+            d_logEntryBuffer.setString("\nfile does not exist!");
             return new WorldMap();
         }
-
         String[] l_split_data;
         while (l_file_reader.hasNextLine()) {
 
             l_split_data = l_file_reader.nextLine().split(" ");
-
+            //loading borders, continents and countries
             try {
                 switch (l_split_data[0]) {
 
@@ -183,8 +194,9 @@ public class MapInterface {
                     case "[countries]" -> loadCountries(l_file_reader, l_worldMap);
                     case "[borders]" -> loadBorders(l_file_reader, l_worldMap);
                 }
-            } catch (Exception e) {
+            } catch (Exception e) { //mapfile is invalid
                 p_gameEngine.d_renderer.renderError("Invalid map file!");
+                d_logEntryBuffer.setString("\nfile does not exist!");
                 new WorldMap();
             }
 
@@ -195,21 +207,21 @@ public class MapInterface {
     /**
      * Loads continents data from a Scanner object into a WorldMap instance.
      *
-     * @param fileReader The Scanner object used for reading data from the input file.
-     * @param p_worldMap The WorldMap instance where the continents will be loaded.
+     * @param p_fileReader The Scanner object used for reading data from the input file.
+     * @param p_worldMap   The WorldMap instance where the continents will be loaded.
      * @throws ContinentAlreadyExistsException If attempting to add a continent that already exists in the WorldMap.
      */
-    public void loadContinents(Scanner fileReader, WorldMap p_worldMap) throws ContinentAlreadyExistsException {
+    public void loadContinents(Scanner p_fileReader, WorldMap p_worldMap) throws ContinentAlreadyExistsException {
 
-        String[] inputData;
-        while (fileReader.hasNextLine()) {
-            inputData = fileReader.nextLine().split(" ");
+        String[] l_inputData;
+        while (p_fileReader.hasNextLine()) {
+            l_inputData = p_fileReader.nextLine().split(" ");
 
-            if (inputData[0].isBlank()) return;
+            if (l_inputData[0].isBlank()) return;
             else {
-                String continentName = inputData[0];
-                int bonus = Integer.parseInt(inputData[1]);
-                p_worldMap.addContinent(continentName, bonus);
+                String l_continentName = l_inputData[0];
+                int l_bonus = Integer.parseInt(l_inputData[1]);
+                p_worldMap.addContinent(l_continentName, l_bonus);
             }
         }
     }
@@ -217,22 +229,21 @@ public class MapInterface {
     /**
      * Loads continents from a file into the world map.
      *
-     * @param fileReader The scanner object used to read the file.
-     * @param p_worldMap The WorldMap object to which continents will be added.
+     * @param p_fileReader The scanner object used to read the file.
+     * @param p_worldMap   The WorldMap object to which continents will be added.
      * @throws ContinentDoesNotExistException if a continent does not exist on the world map.
      * @throws DuplicateCountryException      if a country already exists on the map.
      */
-    public void loadCountries(Scanner fileReader, WorldMap p_worldMap) throws ContinentDoesNotExistException, DuplicateCountryException {
-        String[] inputData;
-        while (fileReader.hasNextLine()) {
-            inputData = fileReader.nextLine().split(" ");
-
-            if (inputData[0].isBlank()) return;
+    public void loadCountries(Scanner p_fileReader, WorldMap p_worldMap) throws ContinentDoesNotExistException, DuplicateCountryException {
+        String[] l_inputData;
+        while (p_fileReader.hasNextLine()) {
+            l_inputData = p_fileReader.nextLine().split(" ");
+            if (l_inputData[0].isBlank()) return;
             else {
-                int countryID = Integer.parseInt(inputData[0]);
-                String countryName = inputData[1];
-                int continentID = Integer.parseInt(inputData[2]);
-                p_worldMap.addCountry(countryName, continentID, countryID);
+                int l_countryID = Integer.parseInt(l_inputData[0]);
+                String l_countryName = l_inputData[1];
+                int l_continentID = Integer.parseInt(l_inputData[2]);
+                p_worldMap.addCountry(l_countryName, l_continentID, l_countryID);
             }
         }
     }
@@ -240,22 +251,21 @@ public class MapInterface {
     /**
      * Loads borders from a file into the world map.
      *
-     * @param fileReader The scanner object used to read the file.
-     * @param p_worldMap The WorldMap object to which borders will be added.
+     * @param p_fileReader The scanner object used to read the file.
+     * @param p_worldMap   The WorldMap object to which borders will be added.
      * @throws CountryDoesNotExistException If a country mentioned in the border data does not exist in the world map.
      */
-    public void loadBorders(Scanner fileReader, WorldMap p_worldMap) throws CountryDoesNotExistException {
-        String[] inputData;
-        while (fileReader.hasNextLine()) {
-
-            inputData = fileReader.nextLine().split(" ");
-            if (inputData[0].isBlank()) return;
+    public void loadBorders(Scanner p_fileReader, WorldMap p_worldMap) throws CountryDoesNotExistException {
+        String[] l_inputData;
+        while (p_fileReader.hasNextLine()) {
+            l_inputData = p_fileReader.nextLine().split(" ");
+            if (l_inputData[0].isBlank()) return;
             else {
-                int sourceCountry = Integer.parseInt(inputData[0]);
-                for (String s : inputData) {
-                    int targetCountry = Integer.parseInt(s);
-                    if (sourceCountry != targetCountry) {
-                        p_worldMap.addBorder(sourceCountry, targetCountry);
+                int l_sourceCountry = Integer.parseInt(l_inputData[0]);
+                for (String l_s : l_inputData) {
+                    int targetCountry = Integer.parseInt(l_s);
+                    if (l_sourceCountry != targetCountry) {
+                        p_worldMap.addBorder(l_sourceCountry, targetCountry);
                     }
                 }
             }
