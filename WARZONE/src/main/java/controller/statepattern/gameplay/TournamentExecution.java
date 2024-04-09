43947070025850d74dd2dbc8be687ee366ecba8b
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-09 12:57:37 -0400

    Update TournamentExecution.java

--- WARZONE/src/main/java/controller/statepattern/gameplay/TournamentExecution.java
+++ WARZONE/src/main/java/controller/statepattern/gameplay/TournamentExecution.java
@@ -6,12 +6,10 @@ import controller.statepattern.End;
 import controller.statepattern.Phase;
 import models.LogEntryBuffer;
 import models.Player;
-import models.orders.Order;
 import models.worldmap.Country;
 import models.worldmap.WorldMap;
 import view.Logger;
 
-import java.sql.SQLOutput;
 import java.util.*;
 
 /**
@@ -22,17 +20,18 @@ public class TournamentExecution extends Phase {
     /**
      * The number of maps used in the tournament.
      */
-    private static int MapNumber =0;
+    private static int MapNumber = 0;
 
     /**
      * Represents a buffer for storing log entries.
      */
-    LogEntryBuffer logEntryBuffer = new LogEntryBuffer();
+    LogEntryBuffer d_logEntryBuffer = new LogEntryBuffer();
 
     /**
      * Represents a logger associated with a log entry buffer.
      */
-    Logger lw = new Logger(logEntryBuffer);
+    Logger d_lw = new Logger(d_logEntryBuffer);
+
     /**
      * Constructor for OrderExecution.
      *
@@ -49,6 +48,7 @@ public class TournamentExecution extends Phase {
     public void displayMenu() {
 
     }
+
     /**
      * This method is intended to advance the game to the next step or phase.
      */
@@ -56,20 +56,21 @@ public class TournamentExecution extends Phase {
     public void next() {
 
     }
+
     /**
      * Retrieves the name of the current phase in the game.
      *
      * @param p_gameEngine The GameEngine object representing the current game state.
      * @return A String representing the name of the current phase.
      */
-    public String getCurrentPhase(GameEngine p_gameEngine)
-    {
-        Phase phase = p_gameEngine.getCurrentState();
-        String l_currClass = String.valueOf(phase.getClass());
+    public String getCurrentPhase(GameEngine p_gameEngine) {
+        Phase l_phase = p_gameEngine.getCurrentState();
+        String l_currClass = String.valueOf(l_phase.getClass());
         int l_index = l_currClass.lastIndexOf(".");
-        System.out.println( l_currClass.substring(l_index+1));
-        return l_currClass.substring(l_index+1);
+        System.out.println(l_currClass.substring(l_index + 1));
+        return l_currClass.substring(l_index + 1);
     }
+
     /**
      * This method is intended to end the game.
      */
@@ -85,6 +86,7 @@ public class TournamentExecution extends Phase {
     public void run() {
         String d_currPhase = getCurrentPhase(d_ge);
 
+<<<<<<< Updated upstream
         if(MapNumber == d_ge.getMapFilesTournament().size()){
             MapNumber =0;
             d_ge.setNumberOfGames(d_ge.getNumberOfGames() + 1);
@@ -100,36 +102,49 @@ public class TournamentExecution extends Phase {
             d_ge.setCurrentMapIndex(MapNumber);
             //System.out.println("Number of Games"+ (d_ge.getNumberOfGames()-1));
             d_ge.setGameResult(d_ge.getNumberOfGames()-1,MapNumber,"in progress");
+=======
+        if (MapNumber == d_ge.getMapFilesTournament().size()) {
+            MapNumber = 0;
+            int l_numberOfGames = d_ge.getNumberGamesTournament();
+            d_ge.setGameResult(l_numberOfGames, MapNumber, "in progress");
+            l_numberOfGames--;
+            d_ge.setNumberGamesTournament(l_numberOfGames);
+            d_ge.setCurrentMapIndex(MapNumber);
+            System.out.println("Number of Games:" + d_ge.getNumberGamesTournament());
+        } else {
+            d_ge.setCurrentMapIndex(MapNumber);
+            d_ge.setGameResult(d_ge.getNumberGamesTournament() - 1, MapNumber, "in progress");
+>>>>>>> Stashed changes
         }
-        if(d_ge.getNumberGamesTournament() == 0 ){
+        if (d_ge.getNumberGamesTournament() == 0) {
             d_ge.setCurrentState(new End(d_ge));
         }
-        MapInterface mp = new MapInterface();
+        MapInterface l_mp = new MapInterface();
         String d_map = d_ge.getMapFilesTournament().get(MapNumber);
         MapNumber++;
         d_ge.d_worldmap = null;
         try {
-            logEntryBuffer.setString("Phase :"+ d_currPhase +"\n"+ " loading map for tournament");      // Log the command entry
-            d_ge.d_worldmap = mp.loadMap(d_ge,d_map);
-        }
-        catch(Exception e){
-            logEntryBuffer.setString("Phase :"+ d_currPhase +"\n"+ " Could Not Load Map. Moving to Next Map");     // Log if map loading fails
+            d_logEntryBuffer.setString("Phase :" + d_currPhase + "\n" + " loading map for tournament");      // Log the command entry
+            d_ge.d_worldmap = l_mp.loadMap(d_ge, d_map);
+        } catch (Exception e) {
+            d_logEntryBuffer.setString("Phase :" + d_currPhase + "\n" + " Could Not Load Map. Moving to Next Map");     // Log if map loading fails
             System.out.println(e);
         }
-        if(!d_ge.d_worldmap.validateMap()){
+        if (!d_ge.d_worldmap.validateMap()) {
             d_ge.d_renderer.renderError("Invalid Map! Cannot load into game");
             d_ge.d_worldmap = new WorldMap();
-            logEntryBuffer.setString("Phase :"+ d_currPhase +"\n"+ " Tournament Map is Invalid!");
+            d_logEntryBuffer.setString("Phase :" + d_currPhase + "\n" + " Tournament Map is Invalid!");
         }
-        logEntryBuffer.setString("Phase :"+ d_currPhase +"\n"+ " Tournament Map Loaded");
+        d_logEntryBuffer.setString("Phase :" + d_currPhase + "\n" + " Tournament Map Loaded");
 
-        if(assignCountries(d_ge,d_currPhase)){
-            logEntryBuffer.setString("Phase :"+d_currPhase+"\n"+ "Countries assigned to Players");
+        if (assignCountries(d_ge, d_currPhase)) {
+            d_logEntryBuffer.setString("Phase :" + d_currPhase + "\n" + "Countries assigned to Players");
         }
         System.out.println(d_ge);
         d_ge.setCurrentState(new Reinforcement(d_ge));
 
     }
+
     /**
      * Assigns countries to players in the tournament phase of the game.
      *
@@ -137,39 +152,39 @@ public class TournamentExecution extends Phase {
      * @param p_currPhase  The current phase of the game.
      * @return true if countries are successfully assigned to players, false otherwise.
      */
-    private boolean assignCountries(GameEngine p_gameEngine,String p_currPhase) {
-        logEntryBuffer.setString("Phase :"+ p_currPhase +"\n"+ " Assigning Countries to players in Tournament");
+    private boolean assignCountries(GameEngine p_gameEngine, String p_currPhase) {
+        d_logEntryBuffer.setString("Phase :" + p_currPhase + "\n" + " Assigning Countries to players in Tournament");
         for (Player l_player : p_gameEngine.d_players) {
             l_player.getAssignedCountries().clear();
         }
-        if(p_gameEngine.d_players.size() == 0){
+        if (p_gameEngine.d_players.isEmpty()) {
             p_gameEngine.d_renderer.renderError("Add atleast one player before assigning");
-            logEntryBuffer.setString("Phase :"+ p_currPhase +"\n"+ "Command: assigncountries Not Executed  || Must add at least one player before assigning countries");
+            d_logEntryBuffer.setString("Phase :" + p_currPhase + "\n" + "Command: assigncountries Not Executed  || Must add at least one player before assigning countries");
             return false;
         }
-        if(p_gameEngine.d_worldmap.getCountries().size()==0){
+        if (p_gameEngine.d_worldmap.getCountries().isEmpty()) {
             p_gameEngine.d_renderer.renderError(" Empty map Please load a Valid Map");
-            logEntryBuffer.setString("Phase :"+ p_currPhase +"\n"+ " Executed Command: assigncountries Not Executed|| Load a valid map!");
+            d_logEntryBuffer.setString("Phase :" + p_currPhase + "\n" + " Executed Command: assigncountries Not Executed|| Load a valid map!");
             return false;
         }
-        HashMap<Integer, Country> map = p_gameEngine.d_worldmap.getD_countries();
-        Set<Integer> l_countryIDSet = map.keySet();
+        HashMap<Integer, Country> l_map = p_gameEngine.d_worldmap.getD_countries();
+        Set<Integer> l_countryIDSet = l_map.keySet();
         ArrayList<Integer> l_countryIDList = new ArrayList<>(l_countryIDSet);
 
-        int total_players = p_gameEngine.d_players.size();
-        int playerNumber = 0;
+        int l_total_players = p_gameEngine.d_players.size();
+        int l_playerNumber = 0;
         while (!l_countryIDList.isEmpty()) {
-            Random rand = new Random();
-            int randomIndex = rand.nextInt(l_countryIDList.size());
-            int l_randomCountryID = l_countryIDList.get(randomIndex);
-            l_countryIDList.remove(randomIndex);
-            if ((playerNumber % total_players == 0) && playerNumber != 0) {
-                playerNumber = 0;
+            Random l_rand = new Random();
+            int l_randomIndex = l_rand.nextInt(l_countryIDList.size());
+            int l_randomCountryID = l_countryIDList.get(l_randomIndex);
+            l_countryIDList.remove(l_randomIndex);
+            if ((l_playerNumber % l_total_players == 0) && l_playerNumber != 0) {
+                l_playerNumber = 0;
             }
-            Country country = map.get(l_randomCountryID);
-            country.setCountryPlayerID(p_gameEngine.d_players.get(playerNumber).getPlayerId());
-            p_gameEngine.d_players.get(playerNumber).setAssignedCountries(l_randomCountryID);
-            playerNumber++;
+            Country l_country = l_map.get(l_randomCountryID);
+            l_country.setCountryPlayerID(p_gameEngine.d_players.get(l_playerNumber).getPlayerId());
+            p_gameEngine.d_players.get(l_playerNumber).setAssignedCountries(l_randomCountryID);
+            l_playerNumber++;
         }
 
         System.out.println("Assigning of Countries Done");
@@ -188,6 +203,6 @@ public class TournamentExecution extends Phase {
     }
 
 
-    }
+}
 
 
