48afcb1203cb57995f38b157e934e1de4b0d85b6
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-09 12:57:03 -0400

    Update MapFileManagementDriver.java

--- WARZONE/src/main/java/controller/MapFileManagement/MapFileManagementDriver.java
+++ WARZONE/src/main/java/controller/MapFileManagement/MapFileManagementDriver.java
@@ -10,28 +10,28 @@ public class MapFileManagementDriver {
 
     /**
      * The main method to run the driver program.
+     *
      * @param args Command-line arguments (not used).
      */
-    public static void main(String[] args){
-        GameEngine ge = new GameEngine();
-        MapInterface mp = new MapInterface();
-        MapFileLoader mfl = new MapFileLoader(ge, "usa9.map");
-        System.out.println(mfl.fileLoaded());
-        System.out.println(mfl.isConquest());
-        mfl = new MapFileLoader(ge, "Africa.map");
-        ConquestMapInterface cmi = new ConquestMapInterface();
-        ge.d_worldmap = cmi.loadConquestMap(ge,mfl);
+    public static void main(String[] args) {
+        GameEngine l_ge = new GameEngine();
+        MapInterface l_mp = new MapInterface();
+        MapFileLoader mfl = new MapFileLoader(l_ge, "usa9.map");
+        System.out.println(mfl.fileLoaded()); //checking if map file was loaded
+        System.out.println(mfl.isConquest()); //checking if loaded map is in conquest format
+        mfl = new MapFileLoader(l_ge, "Africa.map");
+        ConquestMapInterface l_cmi = new ConquestMapInterface();
+        l_ge.d_worldmap = l_cmi.loadConquestMap(l_ge, mfl); //load conquest map
         try {
-            mp.saveMap(ge, "AfricaDomination.map");
-        }
-        catch (Exception e){
+            l_mp.saveMap(l_ge, "AfricaDomination.map");
+        } catch (Exception e) {
             System.out.println(e.toString());
         }
-        ge.d_renderer.showMap(false);
-        mfl = new MapFileLoader(ge, "usa9.map");
+        l_ge.d_renderer.showMap(false);
+        mfl = new MapFileLoader(l_ge, "usa9.map");
 
-        ge.d_worldmap = mp.loadMap(ge, mfl);
-        cmi.saveConquestMap(ge,"usa9Conquest.map");
+        l_ge.d_worldmap = l_mp.loadMap(l_ge, mfl);
+        l_cmi.saveConquestMap(l_ge, "usa9Conquest.map"); //saving conquest map
 
 
     }
