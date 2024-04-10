3ff6bfdce4ec535e6c02757511cf7881d7a2aad0
Shashidhar Krovvidi <shashidhar.krovvidi@mail.concordia.ca>
2024-04-09 22:36:12 -0400

    fixing issueOrderCommandsTest

--- WARZONE/src/test/java/controller/middleware/commands/IssueOrderCommandsTest.java
+++ WARZONE/src/test/java/controller/middleware/commands/IssueOrderCommandsTest.java
@@ -1,10 +1,14 @@
 package controller.middleware.commands;
 
 import controller.GameEngine;
+import controller.statepattern.Phase;
+import controller.statepattern.gameplay.IssueOrder;
 import models.Player;
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 /**
  * The IssueOrderCommandsTest class contains unit tests for the validation of various order commands.
@@ -15,6 +19,12 @@ public class IssueOrderCommandsTest {
      * Represents an instance of the game engine.
      */
     GameEngine d_ge = new GameEngine();
+    /**
+     * current phase
+     */
+    Phase d_phase = new IssueOrder(d_ge);
+
+
 
     /**
      * Represents an instance of a player named "Devdutt" associated with the game engine.
@@ -31,6 +41,11 @@ public class IssueOrderCommandsTest {
         assertFalse(l_obj.validateCommand(d_ge));
     }
 
+    @Before
+    public void setUp()
+    {
+        d_ge.setCurrentState(d_phase);
+    }
 
     /**
      * Tests the validation of the Bomb command with no parameters.
@@ -43,13 +58,23 @@ public class IssueOrderCommandsTest {
     }
 
     /**
+     * Tests the validation of the Bomb command with no parameters.
+     */
+    @Test
+    public void testValidBomb1() {
+        String l_cmd = "bomb PandaLand";
+        IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
+        assertTrue(l_obj.validateCommand(d_ge));
+    }
+
+    /**
      * Tests the validation of the Bomb command with one parameter.
      */
     @Test
     public void testBomb2() {
         String l_cmd = "bomb piyush";
         IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
-        assertFalse(l_obj.validateCommand(d_ge));
+        assertTrue(l_obj.validateCommand(d_ge));
     }
 
     /**
@@ -77,9 +102,9 @@ public class IssueOrderCommandsTest {
      */
     @Test
     public void testBlockade2() {
-        String l_cmd = "blockade piyush";
+        String l_cmd = "blockade Piyush";
         IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
-        assertFalse(l_obj.validateCommand(d_ge));
+        assertTrue(l_obj.validateCommand(d_ge));
     }
 
     /**
@@ -109,7 +134,7 @@ public class IssueOrderCommandsTest {
     public void testNegotiate2() {
         String l_cmd = "negotiate piyush";
         IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
-        assertFalse(l_obj.validateCommand(d_ge));
+        assertTrue(l_obj.validateCommand(d_ge));
     }
 
     /**
@@ -149,7 +174,7 @@ public class IssueOrderCommandsTest {
     public void testDeploy3() {
         String l_cmd = "deploy India 300";
         IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
-        assertFalse(l_obj.validateCommand(d_ge));
+        assertTrue(l_obj.validateCommand(d_ge));
     }
 
     /**
@@ -189,7 +214,7 @@ public class IssueOrderCommandsTest {
     public void testAdvance3() {
         String l_cmd = "advance India Bangladesh 300";
         IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
-        assertFalse(l_obj.validateCommand(d_ge));
+        assertTrue(l_obj.validateCommand(d_ge));
     }
 
     /**
@@ -249,7 +274,7 @@ public class IssueOrderCommandsTest {
     public void testAirlift3() {
         String l_cmd = "airlift India Bangladesh 300";
         IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
-        assertFalse(l_obj.validateCommand(d_ge));
+        assertTrue(l_obj.validateCommand(d_ge));
     }
 
     /**
@@ -278,6 +303,66 @@ public class IssueOrderCommandsTest {
     @Test
     public void testAirlift6() {
         String l_cmd = "airlift India Bangladesh Nepal";
+        IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
+        assertFalse(l_obj.validateCommand(d_ge));
+    }
+
+    /**
+     * Tests the validation of the valid savegame command
+     */
+    @Test
+    public void testSaveGame1() {
+        String l_cmd = "savegame     devdutt";
+        IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
+        assertTrue(l_obj.validateCommand(d_ge));
+    }
+
+    /**
+     * Tests the validation of the invalid savegame command
+     */
+    @Test
+    public void testSaveGame2() {
+        String l_cmd = "savegame     ";
+        IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
+        assertFalse(l_obj.validateCommand(d_ge));
+    }
+
+    /**
+     * Tests the validation of the invalid savegame command
+     */
+    @Test
+    public void testSaveGame3() {
+        String l_cmd = "savegame    shashi pikachu ";
+        IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
+        assertFalse(l_obj.validateCommand(d_ge));
+    }
+
+    /**
+     * Tests the validation of the valid loadgame command
+     */
+    @Test
+    public void loadGame1() {
+        String l_cmd = "loadgame     devdutt";
+        IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
+        assertTrue(l_obj.validateCommand(d_ge));
+    }
+
+    /**
+     * Tests the validation of the invalid loadgame command
+     */
+    @Test
+    public void loadGame2() {
+        String l_cmd = "loadgame     ";
+        IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
+        assertFalse(l_obj.validateCommand(d_ge));
+    }
+
+    /**
+     * Tests the validation of the invalid loadgame command
+     */
+    @Test
+    public void loadGame3() {
+        String l_cmd = "loadgame    shashi pikachu ";
         IssueOrderCommands l_obj = new IssueOrderCommands(l_cmd, d_player);
         assertFalse(l_obj.validateCommand(d_ge));
     }
